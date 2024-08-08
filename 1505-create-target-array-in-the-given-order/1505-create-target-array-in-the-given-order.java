c833f303f050810f0d0798cfb4ce9c1550f174f5
Rohan kumar jha <115976070+RohanKumarJha@users.noreply.github.com>
2024-08-08 10:53:03 +0530

    Time: 0 ms (100%), Space: 41.8 MB (45.58%) - LeetHub

--- 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
+++ 1505-create-target-array-in-the-given-order/1505-create-target-array-in-the-given-order.java
@@ -5,7 +5,7 @@ class Solution {
             newArr.add(index[i],nums[i]);
         }
         int[] arr = new int[index.length];
-        for(int i=0; i<arr.length; i++) {
+        for(int i=0; i<index.length; i++) {
             arr[i] = newArr.get(i);
         } return arr;
     }
