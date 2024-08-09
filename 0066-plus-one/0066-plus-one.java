a187bae53125b02bb8d45a3013b816fca6b1dd0c
Rohan kumar jha <115976070+RohanKumarJha@users.noreply.github.com>
2024-08-09 12:19:14 +0530

    Time: 1 ms (5.99%), Space: 41.8 MB (43.68%) - LeetHub

---
+++ 0066-plus-one/0066-plus-one.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] plusOne(int[] digits) {
+        List<Integer> li = new ArrayList<> ();
+        int k = 1;
+        for(int i=digits.length-1; i>=0; i--) {
+            if(i == digits.length-1) {
+                li.add(0, (digits[i] + k)%10);
+                k= (digits[i] +k) / 10;
+            } else {
+                li.add(0, (digits[i]+k)%10);
+                k= (digits[i] +k) / 10;
+            } 
+        }
+        while (k > 0) {
+            li.add(0, k % 10);
+            k /= 10;
+        }
+        int[] newArr = new int[li.size()];
+        for(int i=0; i<li.size(); i++) {
+            newArr[i] = li.get(i);
+        } return newArr;
+    }
+}
