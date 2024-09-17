7701f010b25582a0162a270a6961cc32fe2991d4
Rohan kumar jha <115976070+RohanKumarJha@users.noreply.github.com>
2024-09-17 16:25:26 +0530

    Time: 1 ms (23.51%), Space: 41.1 MB (96.35%) - LeetHub

--- 0742-to-lower-case/0742-to-lower-case.java
+++ 0742-to-lower-case/0742-to-lower-case.java
@@ -1,5 +1,19 @@
 class Solution {
     public String toLowerCase(String s) {
-        return s.toLowerCase();
+        StringBuilder sb=new StringBuilder();
+        
+        for(int i=0;i<s.length();i++)
+        {
+            if((int)s.charAt(i)>=65 && (int)s.charAt(i)<=90)
+            {
+                sb.append((char)((int)s.charAt(i)+32));
+            }
+            else{
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+        
+        
     }
 }
