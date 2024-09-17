129ad04ae3511dff1263001414c7301925c7b1b4
Rohan kumar jha <115976070+RohanKumarJha@users.noreply.github.com>
2024-09-17 16:40:43 +0530

    Time: 3 ms (90.78%), Space: 41.4 MB (95.49%) - LeetHub

--- 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
+++ 1823-determine-if-string-halves-are-alike/1823-determine-if-string-halves-are-alike.java
@@ -2,7 +2,7 @@ class Solution {
     public static int half(String s,int i,int n) {
         int count = 0;
         for(int num=i; num<n; num++) {
-            if(s.charAt(num)=='a'| s.charAt(num)=='e'| s.charAt(num)=='i'| s.charAt(num)=='o'| s.charAt(num)=='u'| s.charAt(num)=='A'| s.charAt(num)=='E'| s.charAt(num)=='I'| s.charAt(num)=='O'| s.charAt(num)=='U') {
+            if(s.charAt(num)=='a'|| s.charAt(num)=='e'|| s.charAt(num)=='i'|| s.charAt(num)=='o'|| s.charAt(num)=='u'|| s.charAt(num)=='A'|| s.charAt(num)=='E'|| s.charAt(num)=='I'|| s.charAt(num)=='O'|| s.charAt(num)=='U') {
                 count++;
             }
         } return count;
