4af2c4066d3539ccd088c526284a7c8c2490b2fa
Rohan kumar jha <rohan.jha0107@gmail.com>
2024-10-25 16:00:33 +0530

    Time: 0 ms (100%), Space: 42.4 MB (9.1%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,6 +1,8 @@
 class Solution {
     public void rotate(int[][] matrix) {
         int n = matrix.length;
+
+        // Transpose
         for(int i=0; i<n; i++) {
             for(int j=i; j<n; j++) {
                 if(i!=j) {
@@ -10,10 +12,12 @@ class Solution {
                 }
             }
         }
+
+        // Reverse
         for(int i=0; i<n; i++) {
-            for(int j=0; j<n/2; j++) {
+            for(int j=0; j<n/2; j++){
                 int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-j-1];
+                matrix[i][j] = matrix[i][n-1-j];
                 matrix[i][n-j-1] = temp;
             }
         }
