2d3cf922944b4b731bec33bf16a7b4147f25dae3
Rohan kumar jha <rohan.jha0107@gmail.com>
2024-12-06 10:20:40 +0530

    Time: 1439 ms (5.01%), Space: 53.4 MB (17.29%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,7 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
         int n = nums.length;
-        ArrayList<Integer> arrList = new ArrayList<>();
         for(int i=0; i<n; i++) {
             int count = 0;
             for(int j=0; j<n; j++) {
