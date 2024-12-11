63ee2e4ae756ddb71eea9d295b1f871fe4f1aab5
Rohan kumar jha <rohan.jha0107@gmail.com>
2024-12-11 09:35:07 +0530

    Time: 0 ms (100%), Space: 42.3 MB (41.22%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,11 +1,11 @@
 class Solution {
-    public double myPow(double x, int n) {
-        return binaryExp(x, (long) n);
+    public double ans(double x,long n) {
+        if(n==0) return 1;
+        if(n<0) return 1.0/ans(x,-n);
+        if(n%2 == 0) return ans(x*x, n/2);
+        else return x*ans(x*x, (n-1)/2);
     }
-    private double binaryExp(double x, long n) {
-        if (n == 0) return 1;
-        if (n < 0) return 1.0 / binaryExp(x, -n);
-        if (n % 2 == 1) return x * binaryExp(x * x, (n - 1) / 2);
-        else return binaryExp(x * x, n / 2);
+    public double myPow(double x, int n) {
+        return ans(x, (long) n);
     }
 }
