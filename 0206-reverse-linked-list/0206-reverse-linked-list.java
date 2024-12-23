e8fe059c834b76446ab5e4a3298cf8381fb70258
Rohan kumar jha <rohan.jha0107@gmail.com>
2024-12-23 12:12:49 +0530

    Time: 0 ms (100%), Space: 42.5 MB (52.89%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.java
+++ 0206-reverse-linked-list/0206-reverse-linked-list.java
@@ -13,7 +13,7 @@ class Solution {
         ListNode prev=null, curr=head, agla=null;
         while(curr != null) {
             agla = curr.next;
-            curr.next = prev;
+            curr.next =prev;
             prev = curr;
             curr = agla;
         } return prev;
