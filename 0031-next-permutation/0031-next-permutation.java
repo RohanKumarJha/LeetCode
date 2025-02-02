8414b1a7387b658fb7f5ce4d89fc9c05d7342e3b
Rohan kumar jha <rohan.jha0107@gmail.com>
2025-02-02 09:29:23 +0530

    Time: 0 ms (100%), Space: 43.5 MB (11.85%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,32 +1,34 @@
 class Solution {
-    void swap(int[] nums,int start,int end) {
+    public void swap(int[] nums,int start,int end) {
         int temp = nums[start];
         nums[start] = nums[end];
         nums[end] = temp;
     }
-    void reverse(int[] nums,int start,int end) {
+    public void reverse(int[] nums,int start,int end) {
         while(start < end) {
             swap(nums,start,end);
             start++; end--;
         }
     }
     public void nextPermutation(int[] nums) {
+        int len = nums.length;
         int idx1 = -1;
         int idx2 = -1;
 
-        // Find the breaking point
-        for(int i=nums.length-2; i>=0; i--) {
+        // Find breaking point
+        for(int i=len-2; i>=0; i--) {
             if(nums[i] < nums[i+1]) {
                 idx1 = i;
                 break;
             }
         }
 
-        if(idx1 == -1) { // Array is sorted in desc order
-            reverse(nums,0,nums.length-1);
+        // Array is sorted
+        if(idx1 == -1) {
+            reverse(nums,0,len-1);
         } else {
             // Find the minimum element
-            for(int i=nums.length-1; i>idx1; i--) {
+            for(int i=len-1; i>idx1; i--) {
                 if(nums[i]>nums[idx1]) {
                     idx2 = i;
                     break;
