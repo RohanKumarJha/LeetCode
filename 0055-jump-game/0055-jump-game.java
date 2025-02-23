f39a0748ab88e390204d19cb4465dd8c24f6ca51
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-02-23 09:30:00 +0530

    Time: 1 ms (99.92%), Space: 45.7 MB (54.44%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -3,9 +3,7 @@ class Solution {
         int len = nums.length;
         int flag = len-1;
         for(int i=len-2; i>=0; i--) {
-            if(i+nums[i] >= flag) {
-                flag = i;
-            }
+            if(i+nums[i] >= flag) flag = i;
         }
         return (flag==0)?true:false;
     }
