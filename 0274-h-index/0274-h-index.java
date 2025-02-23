35d4981210d7cc7a60624098cc8e9db513b23ba3
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-02-23 11:22:16 +0530

    Time: 0 ms (100%), Space: 41.8 MB (39.83%) - LeetHub

--- 0274-h-index/0274-h-index.java
+++ 0274-h-index/0274-h-index.java
@@ -1,17 +1,18 @@
 class Solution {
     public int hIndex(int[] citations) {
         int len = citations.length;
-        int maxElm = 0;
-        for(int i=0; i<len; i++) {
-            maxElm = Math.max(maxElm, citations[i]);
+        int[] arr = new int[len+1];
+        for(int i=0; i<len+1; i++) {
+            arr[i] = 0;
         }
-        while(maxElm >= 1) {
-            int count = 0;
-            for(int i=0; i<len; i++) {
-                if(citations[i] >= maxElm) count++;
-            }
-            if(count >= maxElm) return maxElm;
-            maxElm--;
+        for(int i=0; i<len; i++) {
+            if(citations[i] > len) arr[len] += 1;
+            else arr[citations[i]] += 1;
+        }
+        int count = 0;
+        for(int i=arr.length-1; i>=0; i--) {
+            count += arr[i];
+            if(count >= i) return i;
         } return 0;
     }
 }
