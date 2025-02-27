0463d933fa5c878edb6e8c5fd705aae89973a338
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-02-27 09:54:25 +0530

    Time: 310 ms (22.49%), Space: 52.8 MB (14.53%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,24 +1,24 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         Arrays.sort(nums);
+        int len = nums.length;
         Set<List<Integer>> result = new HashSet<>();
-        for(int i=0; i<nums.length-2; i++) {
-            List<Integer> answer = new ArrayList<>();
-            int left=i+1, right=nums.length-1;
-            boolean flag = false;
-            while(left < right) {
-                answer = new ArrayList<>();
-                if(nums[i]+nums[left]+nums[right] == 0) {
-                    answer.add(nums[i]);
-                    answer.add(nums[left]);
-                    answer.add(nums[right]);
-                    result.add(answer);
-                    left++;
-                    right--;
-                    flag = true;
-                } else if(nums[i]+nums[left]+nums[right] < 0) left+=1;
-                else right-=1;
-            } 
+        for(int i=0; i<len-2; i++) {
+            int secondIndex=i+1, lastIndex=len-1;
+            while(secondIndex < lastIndex) {
+                if(nums[i]+nums[secondIndex]+nums[lastIndex] == 0) {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[secondIndex]);
+                    list.add(nums[lastIndex]);
+                    result.add(list);
+                    secondIndex++; lastIndex--;
+                } else if(nums[i]+nums[secondIndex]+nums[lastIndex] < 0) {
+                    secondIndex++;
+                } else {
+                    lastIndex--;
+                }
+            }  
         } return new ArrayList<>(result);
     }
 }
