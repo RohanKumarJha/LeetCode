2b2ce869c9caed8e5fdea0e265a9827204ffd640
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-03-10 09:41:55 +0530

    Time: 0 ms (100%), Space: 44.5 MB (83.07%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.java
@@ -11,17 +11,24 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
+        if(head == null) return null;
+        if(head.next == null) return null;
         ListNode slow=head, fast=head;
-        if(head == null || head.next==null) return null;
+        boolean flag = true;
         do {
             slow = slow.next;
             fast = fast.next.next;
-            if(fast==null || fast.next==null) return null;
-        } while(slow != fast);
-        slow = head;
-        while(slow != fast) {
-            slow = slow.next;
-            fast = fast.next;
-        } return slow;
+            if(slow == fast) {
+                fast=head;
+                flag = false;
+                break;
+            }
+        } while(fast!=null && fast.next!=null);
+        if(!flag) {
+            while(fast!=slow) {
+                fast = fast.next;
+                slow = slow.next;
+            } return slow;
+        } return null;
     }
 }
