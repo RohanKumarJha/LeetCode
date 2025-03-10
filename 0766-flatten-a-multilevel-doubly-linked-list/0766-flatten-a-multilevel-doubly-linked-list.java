08264bc0bfae39409652b35e7479cde8dee57faa
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-03-10 11:41:19 +0530

    Time: 0 ms (100%), Space: 41.7 MB (72.8%) - LeetHub

--- 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
+++ 0766-flatten-a-multilevel-doubly-linked-list/0766-flatten-a-multilevel-doubly-linked-list.java
@@ -1,43 +1,24 @@
-/*
-// Definition for a Node.
-class Node {
-    public int val;
-    public Node prev;
-    public Node next;
-    public Node child;
-};
-*/
-
 class Solution {
     private Node flat(Node curr) {
-        if (curr.next == null && curr.child == null) {
-            return curr;
-        }
+        if (curr == null) return null;
 
         if (curr.child != null) {
             Node rightMost = flat(curr.child);
 
             if (curr.next != null) {
-                Node hold = curr.next;
-                rightMost.next = hold;
-                hold.prev = rightMost;
-                curr.next = curr.child;
-                curr.next.prev = curr;
-                curr.child = null;
-                return flat(hold);
-            } else {
-                curr.next = curr.child;
-                curr.next.prev = curr;
-                curr.child = null;
-                return rightMost;
+                rightMost.next = curr.next;
+                curr.next.prev = rightMost;
             }
-        } else {
-            return flat(curr.next);
+
+            curr.next = curr.child;
+            curr.child.prev = curr;
+            curr.child = null;
         }
+
+        return (curr.next != null) ? flat(curr.next) : curr;
     }
 
     public Node flatten(Node head) {
-        if (head == null) return head;
         flat(head);
         return head;
     }
