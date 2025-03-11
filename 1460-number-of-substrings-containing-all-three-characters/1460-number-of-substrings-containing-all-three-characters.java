91a23573883bdb83c9f5a7b423126ce8ca4a2f96
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-03-11 08:36:32 +0530

    Time: 122 ms (5.61%), Space: 45.1 MB (23.35%) - LeetHub

--- 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/1460-number-of-substrings-containing-all-three-characters.java
@@ -1,17 +1,37 @@
 class Solution {
-    public int numberOfSubstrings(String s) { 
-    int[] count = new int[3]; 
-        int left = 0; 
-        int result = 0;
 
-        for (int i = 0; i < s.length(); i++) {
-            count[s.charAt(i) - 'a']++;
-            while (count[0] > 0 && count[1] > 0 && count[2] > 0) {
-                result += s.length() - i; 
-                count[s.charAt(left) - 'a']--; 
-                left++; 
+    public int numberOfSubstrings(String s) {
+        int len = s.length();
+        int left = 0, right = 0;
+        // Track frequency of a, b, c
+        int[] freq = new int[3];
+        int total = 0;
+
+        while (right < len) {
+            // Add character at right pointer to frequency array
+            char curr = s.charAt(right);
+            freq[curr - 'a']++;
+
+            // While we have all required characters
+            while (hasAllChars(freq)) {
+                // All substrings from current window to end are valid
+                // Add count of these substrings to result
+                total += len - right;
+
+                // Remove leftmost character and move left pointer
+                char leftChar = s.charAt(left);
+                freq[leftChar - 'a']--;
+                left++;
             }
+
+            right++;
         }
-        return result; 
+
+        return total;
+    }
+
+    private boolean hasAllChars(int[] freq) {
+        // Check if we have at least one of each character
+        return freq[0] > 0 && freq[1] > 0 && freq[2] > 0;
     }
 }
