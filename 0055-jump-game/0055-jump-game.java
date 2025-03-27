d18310b633b9e647bf6d4c7b5ac96503cde8eb61
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-03-27 10:04:13 +0530

    Time: 1 ms (99.91%), Space: 45.7 MB (70.3%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,15 +1,13 @@
 class Solution {
     public boolean canJump(int[] nums) {
         int len = nums.length;
-        if(len == 1) return true;
         int flag = len-1;
         int start = len-2;
-        while(start > 0) {
+        while(start >= 0) {
             if(nums[start]+start >= flag) {
                 flag = start;
             } start--;
-        }
-        if(start+nums[start] >= flag) return true;
+        } if(flag == 0) return true;
         return false;
     }
 }
