6e987e7582fe0159f1f273aa427d691e57a7e657
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-03-30 08:57:38 +0530

    Time: 12 ms (44.44%), Space: 45.1 MB (30.02%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -3,8 +3,7 @@ class Solution {
         s = s.toLowerCase().replaceAll("[^a-z0-9]","");
         int start=0, end=s.length()-1;
         while(start < end) {
-            if(s.charAt(start) != s.charAt(end)) return false;
-            start++; end--;
+            if(s.charAt(start++) != s.charAt(end--)) return false;
         } return true;
     }
 }
