485000d78fdbad0553b497210aaec62e2b96a3ce
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-03-31 11:11:40 +0530

    Time: 1 ms (82.71%), Space: 41.7 MB (71.34%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,24 +1,28 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        int len1=word1.length(), len2=word2.length();
-        if(len1 == 0) return word2;
-        if(len2 == 0) return word1;
-        int pos1=0, pos2=0;
+        if(word1.length() == 0) return word2;
+        if(word2.length() == 0) return word1;
         StringBuilder sb = new StringBuilder();
+        int pos1=0, pos2=0;
         boolean flag = false;
-        while(pos1!=len1 || pos2!=len2) {
-            if(pos1 != len1 & flag==false) {
-                sb.append(word1.charAt(pos1++));
-                flag = true;
-            } else if(pos2 != len2 & flag==true) {
-                sb.append(word2.charAt(pos2++));
-                flag = false;
-            } else if(pos1 == len1) {
+        while(pos1!=word1.length() | pos2!=word2.length()) {
+            if(pos1 == word1.length()) {
                 sb.append(word2.substring(pos2));
-                pos2 = len2;
-            } else if(pos2 == len2) {
+                pos2++;
+                break;
+            } else if(pos2 == word2.length()) {
                 sb.append(word1.substring(pos1));
-                pos1 = len1;
+                pos1++;
+                break;
+            }
+            if(!flag) {
+                sb.append(word1.charAt(pos1));
+                pos1++;
+                flag = true;
+            } else {
+                sb.append(word2.charAt(pos2));
+                pos2++;
+                flag = false;
             }
         } return sb.toString();
     }
