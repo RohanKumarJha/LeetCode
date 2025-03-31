749c564421cf02c1dad0e7e7a4b821ca7e384877
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-03-31 11:55:54 +0530

    Time: 4 ms (11.97%), Space: 42.2 MB (77.77%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.java
@@ -1,11 +1,24 @@
 class Solution {
     public String gcdOfStrings(String str1, String str2) {
-        if(!(str1+str2).equals(str2+str1)) return "";
-        int len1=str1.length(), len2=str2.length();
-        while(len1 % len2 != 0) {
-            int temp = len1%len2;
-            len1 = len2;
-            len2 = temp;
-        } return str2.substring(0,len2);
+        if(!(str1.concat(str2).equals(str2.concat(str1)))) return "";
+        int pos1 = 0, pos2 = 0;
+        for(int i=0; i<str1.length(); i++) {
+        // if first string are large
+            if(pos2 == str2.length()) pos2=0;
+        // if second string are large
+            if(pos1 == str1.length() && pos2!=str2.length()) return "";
+        // if Both strings are equal
+            if(str1.charAt(pos1)!=str2.charAt(pos2)) return "";
+        // if string divisible by each other
+            if(pos1==str1.length() & pos2==str2.length()) return str2;
+            pos1++; pos2++;
+        }
+        pos1 = str1.length(); pos2=str2.length();
+        while(pos1%pos2!=0) {
+            int temp = pos1%pos2;
+            pos1 = pos2;
+            pos2 = temp;
+        } 
+        return str2.substring(0,pos2);
     }
 }
