96462fc28032a5afb7fc030cff4783a5c62298c9
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-05 09:17:14 +0530

    Time: 0 ms (100%), Space: 42.9 MB (87.62%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,42 +1,45 @@
 class Solution {
+    // Reverse the array
+    public void reverse(int[] nums,int start,int end) {
+        while(start < end) {
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++; end--;
+        }
+    }
+
+    // Swapping method
     public void swap(int[] nums,int start,int end) {
         int temp = nums[start];
         nums[start] = nums[end];
         nums[end] = temp;
     }
-    public void reverse(int[] nums,int start,int end) {
-        while(start < end) {
-            swap(nums,start,end);
-            start++; end--;
-        }
-    }
+
     public void nextPermutation(int[] nums) {
         int len = nums.length;
-        int idx1 = -1;
-        int idx2 = -1;
-
-        // Find breaking point
-        for(int i=len-2; i>=0; i--) {
-            if(nums[i] < nums[i+1]) {
-                idx1 = i;
+        
+        // Find the pivot
+        int pivot = Integer.MIN_VALUE;
+        for(int i=len-1; i>0; i--) {
+            if(nums[i] > nums[i-1]) {
+                pivot = i-1;
                 break;
             }
         }
 
-        // Array is sorted
-        if(idx1 == -1) {
+        // If array is sorted in decreasing order - reverse the array
+        if(pivot == Integer.MIN_VALUE) {
             reverse(nums,0,len-1);
         } else {
-            // Find the minimum element
-            for(int i=len-1; i>idx1; i--) {
-                if(nums[i]>nums[idx1]) {
-                    idx2 = i;
-                    break;
-                }
+            // Find the element which is just larger than pivot
+            int start=pivot+1, end=len-1;
+            while(nums[end] <= nums[pivot] ) {
+                end--;
             }
-            // swap idx1 to idx2
-            if(idx2 != -1) swap(nums,idx1,idx2);
-            reverse(nums,idx1+1,nums.length-1);
+            swap(nums,pivot,end);
+            reverse(nums,pivot+1,len-1);
         }
+
     }
 }
