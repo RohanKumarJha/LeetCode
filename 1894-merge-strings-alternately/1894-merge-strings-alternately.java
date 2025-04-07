c7b6f2ee18ece00ff497309c9500ce5b16fb4b3b
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-07 09:07:36 +0530

    Time: 1 ms (82.56%), Space: 41.6 MB (85.04%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -3,26 +3,24 @@ class Solution {
         if(word1.length() == 0) return word2;
         if(word2.length() == 0) return word1;
         StringBuilder sb = new StringBuilder();
+        char[] chArr1 = word1.toCharArray();
+        char[] chArr2  =word2.toCharArray();
         int pos1=0, pos2=0;
         boolean flag = false;
         while(pos1!=word1.length() & pos2!=word2.length()) {
             if(!flag) {
-                sb.append(word1.charAt(pos1));
-                pos1++;
+                sb.append(chArr1[pos1++]);
                 flag = true;
             } else {
-                sb.append(word2.charAt(pos2));
-                pos2++;
+                sb.append(chArr2[pos2++]);
                 flag = false;
             }
         }
         while(pos1!=word1.length()) {
-            sb.append(word1.charAt(pos1));
-            pos1++;
+            sb.append(chArr1[pos1++]);
         }
         while(pos2!=word2.length()) {
-            sb.append(word2.charAt(pos2));
-            pos2++;
+            sb.append(chArr2[pos2++]);
         } return sb.toString();
     }
 }
