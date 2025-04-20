7d7004347e21fe9711dbabec7280a010a2c93243
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-20 18:48:37 +0530

    Time: 0 ms (100%), Space: 41.7 MB (71.96%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -1,26 +1,16 @@
 class Solution {
     public String mergeAlternately(String word1, String word2) {
-        if(word1.length() == 0) return word2;
-        if(word2.length() == 0) return word1;
         StringBuilder sb = new StringBuilder();
-        char[] chArr1 = word1.toCharArray();
-        char[] chArr2  =word2.toCharArray();
-        int pos1=0, pos2=0;
-        boolean flag = false;
-        while(pos1!=word1.length() & pos2!=word2.length()) {
-            if(!flag) {
-                sb.append(chArr1[pos1++]);
-                flag = true;
-            } else {
-                sb.append(chArr2[pos2++]);
-                flag = false;
-            }
+        int pointer1=0, pointer2=0;
+        while(pointer1!=word1.length() && pointer2!=word2.length()) {
+            sb.append(word1.charAt(pointer1)).append(word2.charAt(pointer2));
+            pointer1++; pointer2++;
         }
-        while(pos1!=word1.length()) {
-            sb.append(chArr1[pos1++]);
+        if(pointer1!=word1.length()) {
+            sb.append(word1.substring(pointer1,word1.length()));
         }
-        while(pos2!=word2.length()) {
-            sb.append(chArr2[pos2++]);
+        if(pointer2!=word2.length()) {
+            sb.append(word2.substring(pointer2,word2.length()));
         } return sb.toString();
     }
 }
