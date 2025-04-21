ab6aed9ecc23f0d3712b34317cddc3e094a6cdaa
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-21 20:11:52 +0530

    Time: 0 ms (100%), Space: 41.7 MB (85.07%) - LeetHub

--- 1894-merge-strings-alternately/1894-merge-strings-alternately.java
+++ 1894-merge-strings-alternately/1894-merge-strings-alternately.java
@@ -3,14 +3,13 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int pointer1=0, pointer2=0;
         while(pointer1!=word1.length() && pointer2!=word2.length()) {
-            sb.append(word1.charAt(pointer1)).append(word2.charAt(pointer2));
-            pointer1++; pointer2++;
+            sb.append(word1.charAt(pointer1++)).append(word2.charAt(pointer2++));
         }
-        if(pointer1!=word1.length()) {
-            sb.append(word1.substring(pointer1,word1.length()));
-        }
-        if(pointer2!=word2.length()) {
+        if(pointer1 == word1.length()) {
             sb.append(word2.substring(pointer2,word2.length()));
+        }
+        if(pointer2 == word2.length()) {
+            sb.append(word1.substring(pointer1,word1.length()));
         } return sb.toString();
     }
 }
