a8864a3046882515d043195c812b72840bc2f231
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-28 09:52:12 +0530

    Time: 1 ms (100%), Space: 44.1 MB (98.68%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -10,34 +10,41 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode head = new ListNode();
-        ListNode temp = head;
-        int carry=0;
-        while(l1!=null && l2!=null) {
-            ListNode curr = new ListNode(((l1.val)+(l2.val)+carry)%10);
-            temp.next = curr;
-            temp = temp.next;
-            carry = ((l1.val)+(l2.val)+carry)/10;
+        ListNode result = new ListNode();
+        ListNode temp = result;
+        int carry = 0;
+        int sum = 0;
+        while(l1 != null && l2 != null) {
+            sum = carry+l1.val+l2.val;
+            ListNode answer = new ListNode(sum % 10);
+            temp.next = answer;
+            carry = sum/10;
             l1 = l1.next;
             l2 = l2.next;
-        }
-        while(l1!=null) {
-            ListNode curr = new ListNode(((l1.val)+carry)%10);
-            temp.next = curr;
             temp = temp.next;
-            carry = ((l1.val)+carry)/10;
+        }
+        while(l1 != null) {
+            sum = carry+l1.val;
+            ListNode answer = new ListNode(sum % 10);
+            temp.next = answer;
+            carry = sum/10;
             l1 = l1.next;
-        }
-        while(l2!=null) {
-            ListNode curr = new ListNode(((l2.val)+carry)%10);
-            temp.next = curr;
             temp = temp.next;
-            carry = ((l2.val)+carry)/10;
-            l2 = l2.next;
         }
-        if(carry > 0) {
-            ListNode curr = new ListNode(carry%10);
-            temp.next = curr;
-        } return head.next;
+        while(l2 != null) {
+            sum = carry+l2.val;
+            ListNode answer = new ListNode(sum % 10);
+            temp.next = answer;
+            carry = sum/10;
+            l2 = l2.next;
+            temp = temp.next;
+        } 
+        if(carry != 0) {
+            sum = carry;
+            ListNode answer = new ListNode(sum % 10);
+            temp.next = answer;
+            carry = sum/10;
+            temp = temp.next;
+        } return result.next;
     }
 }
