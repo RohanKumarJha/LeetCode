84a5cd5d74a4d741132c835c4e7cc8c6b6ebaa25
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-29 11:03:42 +0530

    Time: 1 ms (20.3%), Space: 45.7 MB (93.48%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,30 +1,12 @@
 class Solution {
-    public static int firstOccurence(int[] arr,int target,int s,int e) {
-        int ans = -1;
-        while(s<=e) {
-            int mid = s+(e-s)/2;
-            if(arr[mid] == target) {
-                ans = mid;
-                e = mid-1;
-            } else if(arr[mid]<target) s=mid+1;
-            else e=mid-1;
-        } return ans;
-    }
-    public static int lastOccurence(int[] arr,int target,int s,int e) {
-        int ans = -1;
-        while(s<=e) {
-            int mid = s+(e-s)/2;
-            if(arr[mid] == target) {
-                ans = mid;
-                s = mid+1;
-            } else if(arr[mid]<target) s=mid+1;
-            else e=mid-1;
-        } return ans;
-    }
     public int[] searchRange(int[] nums, int target) {
-        int s=0, e=nums.length-1;
-        int elem1 = firstOccurence(nums,target,s,e);
-        int elem2 = lastOccurence(nums,target,s,e);
-        return new int[]{elem1,elem2};
+        int start=-1, end=-1;
+        for(int i=0; i<nums.length; i++) {
+            if(nums[i] == target) {
+                if(start == -1) {
+                    start = i;
+                } end = i;
+            }
+        } return new int[]{start,end};
     }
 }
