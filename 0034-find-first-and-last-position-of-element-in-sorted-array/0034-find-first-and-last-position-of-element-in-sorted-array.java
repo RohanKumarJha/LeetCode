88a6e38ec419def6906eb266320fb0d8dcaf26ce
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-29 11:18:49 +0530

    Time: 0 ms (100%), Space: 45.9 MB (67.83%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,12 +1,42 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int start=-1, end=-1;
-        for(int i=0; i<nums.length; i++) {
-            if(nums[i] == target) {
-                if(start == -1) {
-                    start = i;
-                } end = i;
+        int firstOccu=-1, lastOccu=-1;
+
+        // Find the first occurence
+        int start=0;
+        int end=nums.length-1;
+        boolean flag = false;
+        while(start <= end) {
+            int mid = start+(end-start)/2;
+            if(nums[mid] == target) {
+                flag = true;
+                end = mid-1;
+            } else if(nums[mid] < target) {
+                start = mid+1;
+            } else {
+                end = mid-1;
             }
-        } return new int[]{start,end};
+        }
+        if(flag) {
+            firstOccu = end+1;
+        } else {
+            return new int[]{-1,-1};
+        }
+
+
+        // Find the last occurence
+        start=0;
+        end=nums.length-1;
+        while(start <= end) {
+            int mid = start+(end-start)/2;
+            if(nums[mid] == target) {
+                start = mid+1;
+            } else if(nums[mid] < target) {
+                start = mid+1;
+            } else {
+                end = mid-1;
+            }
+        } lastOccu = start-1;
+        return new int[]{firstOccu,lastOccu};
     }
 }
