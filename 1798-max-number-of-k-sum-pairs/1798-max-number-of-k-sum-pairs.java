a9d05c9f30774a496a1bcb8a9222a195263465ae
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-29 23:14:26 +0530

    Time: 18 ms (92.96%), Space: 57.1 MB (66.17%) - LeetHub

---
+++ 1798-max-number-of-k-sum-pairs/1798-max-number-of-k-sum-pairs.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int maxOperations(int[] nums, int k) {
+        Arrays.sort(nums);
+        int start=0, end=nums.length-1;
+        int count=0;
+        while(start < end) {
+            int sum = nums[start]+nums[end];
+            if(sum < k) {
+                start++;
+            } else if(sum == k) {
+                count++;
+                start++; end--;
+            } else {
+                end--;
+            }
+        } return count;
+    }
+}
