eab3f27078d4f9115881be0e2b1579a42a8b2fdb
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-04-30 09:33:48 +0530

    Time: 0 ms (100%), Space: 42.4 MB (26.01%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,20 +1,35 @@
 class Solution {
+    public void swap(int[][] matrix,int i,int j) {
+        int temp = matrix[i][j];
+        matrix[i][j] = matrix[j][i];
+        matrix[j][i] = temp;
+    }
+
+    public void reverse(int[][] matrix,int i,int start,int end) {
+        int temp = matrix[i][start];
+        matrix[i][start] = matrix[i][end];
+        matrix[i][end] = temp;
+    }
+
     public void rotate(int[][] matrix) {
-        int row=matrix.length, col=matrix[0].length;
-        for(int i=0; i<row; i++) {
-            for(int j=i; j<col; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+
+        // swap row-col or vice-versa
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=i; j<matrix[0].length; j++) {
+                if(i != j) {
+                    swap(matrix,i,j);
+                }
             }
         }
-        for(int i=0; i<row; i++) {
-            int start=0, end=col-1;
-            while(start < end) {
-                int temp = matrix[i][start];
-                matrix[i][start] = matrix[i][end];
-                matrix[i][end] = temp;
-                start++; end--;
+
+        // now reverse the array
+        for(int i=0; i<matrix.length; i++) {
+            int first = 0;
+            int last = matrix[0].length-1;
+            while(first < last) {
+                reverse(matrix,i,first,last);
+                first++;
+                last--;
             }
         }
     }
