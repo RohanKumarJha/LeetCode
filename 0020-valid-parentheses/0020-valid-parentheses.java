635b6e89248aabf212b94aa81af9404aef16046e
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-05-03 23:52:10 +0530

    Time: 3 ms (41.57%), Space: 42 MB (20.07%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,34 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
-        if(s.length() == 1) return false;
         Stack<Character> st = new Stack<>();
         for(int i=0; i<s.length(); i++) {
-            if(s.charAt(i) == ')') {
-                if(st.isEmpty()) return false;
-                if(st.peek() == '(') {
-                    st.pop();
-                } else {
-                    st.push(')');
-                }
-            } else if(s.charAt(i) == ']') {
-                if(st.isEmpty()) return false;
-                if(st.peek() == '[') {
-                    st.pop();
-                } else {
-                    st.push(']');
-                }
-            } else if(s.charAt(i) == '}') {
-                if(st.isEmpty()) return false;
-                if(st.peek() == '{') {
-                    st.pop();
-                } else {
-                    st.push('}');
+            if(s.charAt(i)=='(' || s.charAt(i)=='[' || s.charAt(i)=='{') {
+                st.push(s.charAt(i));
+            } else if(st.isEmpty()) {
+                if(s.charAt(i)==')' || s.charAt(i)=='}' || s.charAt(i)==']') {
+                    return false;
                 }
             } else {
-                st.push(s.charAt(i));
+                if(s.charAt(i)==')' && st.peek()=='(' && i>0) {
+                    st.pop();
+                } else if(s.charAt(i)=='}' && st.peek()=='{' && i>0) {
+                    st.pop();
+                } else if(s.charAt(i)==']' && st.peek()=='[' && i>0) {
+                    st.pop();
+                } else {
+                    return false;
+                }
             }
         }
-        if(st.isEmpty()) return true;
-        return false;
+        return (st.isEmpty()) ? true : false;
     }
 }
