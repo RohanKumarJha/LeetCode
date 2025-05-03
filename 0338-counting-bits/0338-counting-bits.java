5df8a77c8c423c3474db1d026eaad0c1e024749a
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-05-03 21:25:41 +0530

    Time: 2 ms (94.7%), Space: 47.1 MB (7.54%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.java
+++ 0338-counting-bits/0338-counting-bits.java
@@ -1,23 +1,16 @@
-// Approach -1
 class Solution {
     public int[] countBits(int n) {
-        int[] ans = new int[n + 1];
-        
-        for (int i = 0; i <= n; i++) {
-            int count = 0;
-            int num = i;
-            
-            // Count the number of 1's in the binary representation of i
-            while (num > 0) {
-                if (num % 2 == 1) {
-                    count++;
-                }
-                num = num / 2; // Shift right by dividing by 2
+        int[] dp = new int[n + 1];
+        int sub = 1;
+
+        for (int i = 1; i <= n; i++) {
+            if (sub * 2 == i) {
+                sub = i;
             }
-            
-            ans[i] = count; // Store the count in the ans array
+
+            dp[i] = dp[i - sub] + 1;
         }
-        
-        return ans;
+
+        return dp;        
     }
 }
