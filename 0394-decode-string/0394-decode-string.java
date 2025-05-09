c18638d4586c897087c7cd5b58a5644ef74c2785
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-05-09 22:59:06 +0530

    Time: 5 ms (12.23%), Space: 42.1 MB (14.19%) - LeetHub

--- 0394-decode-string/0394-decode-string.java
+++ 0394-decode-string/0394-decode-string.java
@@ -1,28 +1,23 @@
+import java.util.Stack;
+
 class Solution {
     public String decodeString(String s) {
         Stack<Integer> countStack = new Stack<>();
-        Stack<StringBuilder> stringStack = new Stack<>();
-        StringBuilder currentString = new StringBuilder();
+        Stack<String> stringStack = new Stack<>();
+        String current = "";
         int count = 0;
 
         for (char c : s.toCharArray()) {
-            if (Character.isDigit(c)) {
-                count = count * 10 + (c - '0'); // Handle multi-digit numbers
-            } else if (c == '[') {
+            if (Character.isDigit(c)) count = count * 10 + (c - '0');
+            else if (c == '[') {
                 countStack.push(count);
-                stringStack.push(currentString);
-                currentString = new StringBuilder();
-                count = 0; // Reset count after pushing to stack
-            } else if (c == ']') {
-                StringBuilder decodedString = stringStack.pop();
-                int repeatTimes = countStack.pop();
-                while (repeatTimes-- > 0) {
-                    decodedString.append(currentString);
-                }
-                currentString = decodedString; // Move back to previous level
-            } else {
-                currentString.append(c);
-            }
-        } return currentString.toString();
+                stringStack.push(current);
+                current = "";
+                count = 0;
+            } else if (c == ']') 
+                current = stringStack.pop() + current.repeat(countStack.pop());
+            else current += c;
+        }
+        return current;
     }
 }
