f34e60094a64d07cb64205e8cdfc3f02b4fe0e1d
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-05-29 10:41:45 +0530

    Time: 0 ms (100%), Space: 45.9 MB (84%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,42 +1,36 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        int firstOccu=-1, lastOccu=-1;
+        int[] ans = {-1,-1};
 
-        // Find the first occurence
-        int start=0;
-        int end=nums.length-1;
-        boolean flag = false;
-        while(start <= end) {
-            int mid = start+(end-start)/2;
-            if(nums[mid] == target) {
-                flag = true;
-                end = mid-1;
-            } else if(nums[mid] < target) {
+        int start = search(nums,target,true);
+        int end = search(nums,target,false);
+
+        ans[0] = start;
+        ans[1] = end;
+
+        return ans;
+    }
+    int search(int[] nums,int target,boolean findFirstIndex){
+        int ans = -1;
+        int start = 0;
+        int end = nums.length - 1;
+        while(start<=end){
+            int mid = start + (end-start)/2;
+            if(nums[mid]>target){
+                end = mid - 1;
+            }
+            else if(nums[mid]<target){
                 start = mid+1;
-            } else {
-                end = mid-1;
+            }
+            else{
+                ans = mid;
+                if(findFirstIndex){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
             }
         }
-        if(flag) {
-            firstOccu = end+1;
-        } else {
-            return new int[]{-1,-1};
-        }
-
-
-        // Find the last occurence
-        start=0;
-        end=nums.length-1;
-        while(start <= end) {
-            int mid = start+(end-start)/2;
-            if(nums[mid] == target) {
-                start = mid+1;
-            } else if(nums[mid] < target) {
-                start = mid+1;
-            } else {
-                end = mid-1;
-            }
-        } lastOccu = start-1;
-        return new int[]{firstOccu,lastOccu};
+        return ans;
     }
 }
