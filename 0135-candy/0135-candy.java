67106a40c044f3b1c72db31dbd343d160415eaa8
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-06-02 16:30:48 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0135-candy/0135-candy.java
+++ 0135-candy/0135-candy.java
@@ -1,26 +1,15 @@
 class Solution {
     public int candy(int[] ratings) {
-        int n = ratings.length;
-        int[] candies = new int[n];
-        Arrays.fill(candies, 1);
+        if(ratings.length == 1)  return 1;
+        int result = ratings.length;
+        for(int i=0; i<ratings.length; i++) {
+            if(i==0) {
+                if(i > i+1) result++;
+            } else if(i==ratings.length-1) {
+                if(i > i-1) result++;
+            } else {
 
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
             }
         }
-
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = Math.max(candies[i], candies[i + 1] + 1);
-            }
-        }
-
-        int totalCandies = 0;
-        for (int candy : candies) {
-            totalCandies += candy;
-        }
-
-        return totalCandies;
     }
 }
