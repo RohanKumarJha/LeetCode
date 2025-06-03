9bc5d13c59c44fcef47de4a82cc9698bc647c67b
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-06-03 15:17:19 +0530

    Time: 0 ms (100%), Space: 41.7 MB (27.34%) - LeetHub

--- 0054-spiral-matrix/0054-spiral-matrix.java
+++ 0054-spiral-matrix/0054-spiral-matrix.java
@@ -1,36 +1,42 @@
 class Solution {
     public List<Integer> spiralOrder(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        int rowBegin=0, rowEnd=m-1, colBegin=0, colEnd=n-1;
+        List<Integer> list = new ArrayList<>();
+        int count=0;
+        int minRow=0, maxRow=matrix.length-1;
+        int minCol=0, maxCol=matrix[0].length-1;
+        while(count < matrix.length*matrix[0].length) {
 
-        List<Integer> result = new ArrayList<>();
+            int i=minCol;
+            while(i <= maxCol) {
+                list.add(matrix[minRow][i]);
+                count++;
+                i++;
+            } minRow++;
+            if(count == matrix.length*matrix[0].length) break;
 
-        if (matrix.length == 0) {
-            return result;
-        }
-        while(rowBegin <= rowEnd && colBegin <= colEnd) {
+            i = minRow;
+            while(i <= maxRow) {
+                list.add(matrix[i][maxCol]);
+                count++;
+                i++;
+            } maxCol--;
+            if(count == matrix.length*matrix[0].length) break;
 
-            for(int i=colBegin; i<=colEnd; i++) {
-                result.add(matrix[rowBegin][i]);
-            } rowBegin++;
+            i = maxCol;
+            while(i >= minCol) {
+                list.add(matrix[maxRow][i]);
+                count++;
+                i--;
+            } maxRow--;
+            if(count == matrix.length*matrix[0].length) break;
 
-            for(int i=rowBegin; i<=rowEnd; i++) {
-                result.add(matrix[i][colEnd]);
-            } colEnd--;
-
-            if (rowBegin <= rowEnd) {
-                for(int i=colEnd; i>=colBegin; i--) {
-                result.add(matrix[rowEnd][i]);
-                } rowEnd--;
-            }
-
-            if (colBegin <= colEnd) {
-                for(int i=rowEnd; i>=rowBegin; i--) {
-                result.add(matrix[i][colBegin]);
-                } colBegin++;
-            }
-
-        } return result;
+            i = maxRow;
+            while(i >= minRow) {
+                list.add(matrix[i][minCol]);
+                count++;
+                i--;
+            } minCol++;
+            if(count == matrix.length*matrix[0].length) break;
+        } return list;
     }
 }
