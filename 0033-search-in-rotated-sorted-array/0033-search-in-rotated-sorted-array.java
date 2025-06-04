e9f4d04c5d12e283b556ae3ed95c0b8d43dcaf6c
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-06-04 14:05:25 +0530

    Time: 0 ms (100%), Space: 41.8 MB (92.42%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -3,20 +3,29 @@ class Solution {
         int start=0, end=nums.length-1;
         while(start <= end) {
             int mid = start+(end-start)/2;
-            if(nums[mid] == target) return mid;
-            if(nums[start]<=nums[mid]) {
-                if(nums[start] <= target && target<=nums[mid]) {
-                    end = mid-1;
-                } else {
+            if(nums[mid] == target) {
+                return mid;
+            } else if(nums[start] < nums[end]) {
+                if(nums[mid] < target) {
                     start = mid+1;
+                } else if(nums[mid] > target) {
+                    end = mid-1;
                 }
             } else {
-                if(nums[mid] <= target && target<=nums[end]) {
-                    start = mid+1;
+                if(nums[start] <= nums[mid]) {
+                    if(nums[start]<=target && target<=nums[mid]) {
+                        end = mid;
+                    } else {
+                        start = mid+1;
+                    }
                 } else {
-                    end = mid-1;
+                    if(nums[mid]<=target && target<=nums[end]) {
+                        start = mid;
+                    } else {
+                        end = mid-1;
+                    }
                 }
-            }
+            }    
         } return -1;
     }
 }
