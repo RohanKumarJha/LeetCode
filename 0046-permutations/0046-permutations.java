0f1399b2a0ed2b0b31c550b0c3559c85369ae5c9
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-06-05 08:34:20 +0530

    Time: 2 ms (39.09%), Space: 44.8 MB (19.14%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,32 +1,36 @@
-class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        if (nums.length == 1) {
-            List<Integer> singleList = new ArrayList<>();
-            singleList.add(nums[0]);
-            res.add(singleList);
-            return res;
-        }
 
-        for (int i = 0; i < nums.length; i++) {
-            int n = nums[i];
-            int[] remainingNums = new int[nums.length - 1];
-            int index = 0;
-            for (int j = 0; j < nums.length; j++) {
-                if (j != i) {
-                    remainingNums[index] = nums[j];
-                    index++;
-                }
-            }
-            
-            List<List<Integer>> perms = permute(remainingNums);
-            for (List<Integer> p : perms) {
-                p.add(n);
-            }
-            
-            res.addAll(perms);
-        }
-        
-        return res;        
+public class Solution {
+
+  public List<List<Integer>> permute(int[] nums) {
+
+    List<List<Integer>> resultList = new ArrayList<>();
+
+    backtrack(resultList, new ArrayList<>(), nums);
+    return resultList;
+  }
+
+  private void backtrack(List<List<Integer>> resultList,
+                         ArrayList<Integer> tempList, int[] nums) {
+    // If we match the length, it is a permutation
+    if (tempList.size() == nums.length) {
+      resultList.add(new ArrayList<>(tempList));
+      return;
     }
+
+    for (int number : nums) {
+      // Skip if we get same element
+      if (tempList.contains(number))
+        continue;
+
+      // Add the new element
+      tempList.add(number);
+
+      // Go back to try other element
+      backtrack(resultList, tempList, nums);
+
+      // Remove the element
+      tempList.remove(tempList.size() - 1);
+    }
+  }
+
 }
