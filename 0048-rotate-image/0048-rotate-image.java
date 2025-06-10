f04ba65a8698eda75402d82e67d24e128348f901
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-06-10 10:31:42 +0530

    Time: 0 ms (100%), Space: 42.3 MB (59.67%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,35 +1,21 @@
 class Solution {
-
-    // Transpose swap (matrix[i][j] <-> matrix[j][i])
-    public void swapTranspose(int[][] matrix, int i, int j) {
-        int temp = matrix[i][j];
-        matrix[i][j] = matrix[j][i];
-        matrix[j][i] = temp;
-    }
-
-    // Reverse a row
-    public void reverseRow(int[] row) {
-        int start = 0, end = row.length - 1;
-        while (start < end) {
-            int temp = row[start];
-            row[start] = row[end];
-            row[end] = temp;
-            start++;
-            end--;
-        }
-    }
-
     public void rotate(int[][] matrix) {
-        // Step 1: Transpose
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = i + 1; j < matrix[0].length; j++) {
-                swapTranspose(matrix, i, j);
+        for(int i=0; i<matrix.length; i++) {
+            for(int j=i; j<matrix[0].length; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
             }
         }
 
-        // Step 2: Reverse each row
-        for (int[] row : matrix) {
-            reverseRow(row);
+        for(int i=0; i<matrix.length; i++) {
+            int start=0, end=matrix.length-1;
+            while(start < end) {
+                int temp = matrix[i][start];
+                matrix[i][start] = matrix[i][end];
+                matrix[i][end] = temp;
+                start++; end--;
+            }
         }
     }
 }
