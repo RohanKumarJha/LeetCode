88f5f423dbded15e129d6ba1aaddc80abd01c026
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-06-10 11:59:48 +0530

    Time: 0 ms (100%), Space: 42.2 MB (60.5%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,15 +1,18 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int pos1=m-1, pos2=n-1;
-        for(int i=m+n-1; i>=0; i--) {
-            if(pos2 < 0) {
+        int pointer = m+n-1;
+        int start = m-1, end=n-1;
+        while(pointer >= 0) {
+            if(start == -1) {
+                nums1[pointer--] = nums2[end--];
+            } else if(end == -1) {
                 break;
-            } else if(pos1 < 0) {
-                nums1[i]=nums2[pos2--]; 
-            } else if(nums1[pos1] < nums2[pos2]) {
-                nums1[i]=nums2[pos2--];
             } else {
-                nums1[i]=nums1[pos1--];
+                if(nums1[start] > nums2[end]) {
+                    nums1[pointer--] = nums1[start--];
+                } else {
+                    nums1[pointer--] = nums2[end--];
+                }
             }
         }
     }
