721336ac8c33b7ea89f6ac8ff16fd138cbd43d9d
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-07-03 10:07:21 +0530

    Time: 0 ms (100%), Space: 43.7 MB (10.45%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -1,18 +1,29 @@
 class Solution {
-    public boolean search(int[] arr, int target) {
-        int start=0, end=arr.length-1;
+    public boolean search(int[] nums, int target) {
+        int start=0, end=nums.length-1;
         while(start <= end) {
-            int mid=start+(end-start)/2;
-            if(arr[mid] == target) return true;
-            else if(arr[start] == arr[mid] & arr[mid]==arr[end]) {
+            if(nums[start]==nums[end] && nums[start]!=target) {
                 start++; end--;
-            }
-            else if(arr[start] <= arr[mid]) {
-                if(arr[start]<=target & target<=arr[mid]) end=mid-1;
-                else start=mid+1;
-            } else {
-                if(arr[mid]<=target & target<=arr[end]) start=mid+1;
-                else end=mid-1;
+            } else if(nums[start]==nums[end] && nums[start]==target) {
+                return true;
+            }else {
+                int mid = start+(end-start)/2;
+                if(nums[mid] == target) return true;
+                
+                // Find the shortest side
+                if(nums[start] <= nums[mid]) {
+                    if(nums[start]<=target && target<=nums[mid]) {
+                        end = mid;
+                    } else {
+                        start = mid+1;
+                    }
+                } else if(nums[mid] <= nums[end]) {
+                    if(nums[mid]<=target && target<=nums[end]) {
+                        start = mid;
+                    } else {
+                        end = mid-1;
+                    }
+                }
             }
         } return false;
     }
