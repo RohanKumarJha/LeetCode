df5d1b735d804da1260c4153ce552553c3533de0
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-07-04 09:11:22 +0530

    Time: 851 ms (11.16%), Space: 52.2 MB (31.45%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,24 +4,18 @@ class Solution {
         int size = nums.length;
         Set<List<Integer>> list = new HashSet<>();
         for(int i=0; i<size-2; i++) {
-            List<Integer> newList = new ArrayList<>();
             int start=i+1, end=size-1;
             while(start < end) {
                 if(nums[i]+nums[start]+nums[end] == 0) {
-                    newList.add(nums[i]);
-                    newList.add(nums[start]);
-                    newList.add(nums[end]);
-                    list.add(newList);
-                    newList = new ArrayList<>();
+                    list.add(Arrays.asList(nums[i],nums[start],nums[end]));
                     start++;
+                    end--;
                 } else if(nums[i]+nums[start]+nums[end] < 0) {
                     start++;
                 } else {
                     end--;
                 }
             }
-        }
-        List<List<Integer>> answer = new ArrayList<>(list);
-        return answer;
+        } return new ArrayList<>(list);
     }
 }
