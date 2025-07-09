1a00cd1a0b3a4779067d916c608a3d1d7e2d6c78
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-07-09 16:13:13 +0530

    Time: 13 ms (6.83%), Space: 49 MB (6.62%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,20 +1,33 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        List<int[]> merged = new ArrayList<>();
-        int[] prev = intervals[0];
-
-        for (int i = 1; i < intervals.length; i++) {
-            int[] interval = intervals[i];
-            if (interval[0] <= prev[1]) {
-                prev[1] = Math.max(prev[1], interval[1]);
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        int row = intervals.length;
+        int start=0, end=0;
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i=0; i<row; i++) {
+            if(i == 0) {
+                start = intervals[i][0];
+                end = intervals[i][1];
+            } else if(intervals[i][0]<=end) {
+                end = Math.max(end, intervals[i][1]);
             } else {
-                merged.add(prev);
-                prev = interval;
+                List<Integer> newList = new ArrayList<>();
+                newList.add(start);
+                newList.add(end);
+                list.add(newList);
+                start = intervals[i][0];
+                end = intervals[i][1];
             }
         }
-        merged.add(prev);
-        return merged.toArray(new int[merged.size()][]);         
+        List<Integer> newList = new ArrayList<>();
+        newList.add(start);
+        newList.add(end);
+        list.add(newList);
+
+        int[][] array = new int[list.size()][];
+        for (int i = 0; i < list.size(); i++) {
+            List<Integer> result = list.get(i);
+            array[i] = result.stream().mapToInt(Integer::intValue).toArray();
+        } return array;
     }
 }
