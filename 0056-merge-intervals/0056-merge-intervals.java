d60e0875d4dbf44fc42db1e1771738303241ac2c
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-07-10 13:38:47 +0530

    Time: 9 ms (46.05%), Space: 46.6 MB (72.4%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,33 +1,31 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
         Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
-        int row = intervals.length;
-        int start=0, end=0;
-        List<List<Integer>> list = new ArrayList<>();
-        for(int i=0; i<row; i++) {
-            if(i == 0) {
-                start = intervals[i][0];
-                end = intervals[i][1];
-            } else if(intervals[i][0]<=end) {
+        List<List<Integer>> twodList = new ArrayList<>();
+        int start=intervals[0][0], end=intervals[0][1];
+        for(int i=0; i<intervals.length; i++) {
+            if(end >= intervals[i][0]) {
                 end = Math.max(end, intervals[i][1]);
             } else {
-                List<Integer> newList = new ArrayList<>();
-                newList.add(start);
-                newList.add(end);
-                list.add(newList);
+                List<Integer> list = new ArrayList<>();
+                list.add(start);
+                list.add(end);
+                twodList.add(list);
                 start = intervals[i][0];
                 end = intervals[i][1];
             }
         }
-        List<Integer> newList = new ArrayList<>();
-        newList.add(start);
-        newList.add(end);
-        list.add(newList);
+        List<Integer> list = new ArrayList<>();
+        list.add(start);
+        list.add(end);
+        twodList.add(list);
 
-        int[][] array = new int[list.size()][];
-        for (int i = 0; i < list.size(); i++) {
-            List<Integer> result = list.get(i);
-            array[i] = result.stream().mapToInt(Integer::intValue).toArray();
+        int[][] array = new int[twodList.size()][twodList.get(0).size()];
+        for (int i = 0; i < twodList.size(); i++) {
+            List<Integer> row = twodList.get(i);
+            for (int j = 0; j < row.size(); j++) {
+                array[i][j] = row.get(j);
+            }
         } return array;
     }
 }
