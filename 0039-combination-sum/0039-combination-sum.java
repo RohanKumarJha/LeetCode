1ae3e157ce85cfe1f0289213e069a03141108fa3
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-07-15 09:54:54 +0530

    Time: 2 ms (82.88%), Space: 44.5 MB (92.36%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.java
+++ 0039-combination-sum/0039-combination-sum.java
@@ -1,19 +1,19 @@
 class Solution {
-    private void backTrack(int[] c, List<Integer> comb, int target, int sum, int index, int noOfElement, List<List<Integer>> ans) {
+    private void backTrack(int[] c, List<Integer> comb, int target, int sum, int index, List<List<Integer>> ans) {
         if(sum == target) {
             ans.add(new ArrayList<>(comb));
             return ;
         }
-        if(sum > target || index >= noOfElement) return ;
+        if(sum > target || index >= c.length) return ;
         comb.add(c[index]);
-        backTrack(c, comb, target, sum+c[index], index, noOfElement, ans);
+        backTrack(c, comb, target, sum+c[index], index, ans);
         comb.remove(comb.size()-1);
-        backTrack(c, comb, target, sum, index+1, noOfElement, ans);
+        backTrack(c, comb, target, sum, index+1, ans);
     }
 
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         List<List<Integer>> list = new ArrayList<>();
-        backTrack(candidates, new ArrayList<>(), target, 0, 0, candidates.length, list);
+        backTrack(candidates, new ArrayList<>(), target, 0, 0, list);
         return list;
     }
 }
