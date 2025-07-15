151097301a3744e7ce44c041d3b0e178974d1bc9
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-07-15 10:56:56 +0530

    Time: 4 ms (3.88%), Space: 44.8 MB (18.52%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,36 +1,31 @@
-
-public class Solution {
-
-  public List<List<Integer>> permute(int[] nums) {
-
-    List<List<Integer>> resultList = new ArrayList<>();
-
-    backtrack(resultList, new ArrayList<>(), nums);
-    return resultList;
-  }
-
-  private void backtrack(List<List<Integer>> resultList,
-                         ArrayList<Integer> tempList, int[] nums) {
-    // If we match the length, it is a permutation
-    if (tempList.size() == nums.length) {
-      resultList.add(new ArrayList<>(tempList));
-      return;
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        backtrack(nums, 0, res);
+        return res;
     }
 
-    for (int number : nums) {
-      // Skip if we get same element
-      if (tempList.contains(number))
-        continue;
+    private void backtrack(int[] nums, int idx, List<List<Integer>> res) {
+        if (idx == nums.length) {
+            res.add(new ArrayList<>(Arrays.stream(nums).boxed().toList()));
+            return;
+        }
 
-      // Add the new element
-      tempList.add(number);
-
-      // Go back to try other element
-      backtrack(resultList, tempList, nums);
-
-      // Remove the element
-      tempList.remove(tempList.size() - 1);
+        // Helper function to simulate looping
+        recursiveSwap(nums, idx, idx, res);
     }
-  }
 
+    private void recursiveSwap(int[] nums, int idx, int i, List<List<Integer>> res) {
+        if (i == nums.length) return;
+
+        swap(nums, idx, i);
+        backtrack(nums, idx + 1, res);
+        swap(nums, idx, i); // backtrack
+
+        recursiveSwap(nums, idx, i + 1, res); // next swap candidate
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i]; nums[i] = nums[j]; nums[j] = temp;
+    }
 }
