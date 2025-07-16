c3d7b8a101d764bfe91159a56d7b16e2693ca035
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-07-16 08:43:30 +0530

    Time: 0 ms (100%), Space: 42.8 MB (65.5%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,19 +1,23 @@
 class Solution {
-    public void allSubsets(int[] nums, List<List<Integer>> list, List<Integer> newList, int index) {
+    public void recursiveCall(int[] nums, List<List<Integer>> list, int index, List<Integer> result) {
+        // Base case
         if(index == nums.length) {
-            list.add(new ArrayList<>(newList));
+            list.add(new ArrayList<>(result));
             return ;
         }
-        newList.add(nums[index]);
-        allSubsets(nums, list, newList, index+1);
-        newList.remove(newList.size()-1);
-        allSubsets(nums, list, newList, index+1);
+
+        // Add element
+        result.add(nums[index]);
+        recursiveCall(nums, list, index+1, result);
+
+        // Ignore element
+        result.remove(result.size()-1);
+        recursiveCall(nums, list, index+1, result);
     }
 
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> list = new ArrayList<>();
-        List<Integer> newList = new ArrayList<>();
-        allSubsets(nums,list,newList, 0);
+        recursiveCall(nums, list, 0, new ArrayList<>());
         return list;
     }
 }
