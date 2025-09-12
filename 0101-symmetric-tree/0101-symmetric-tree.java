76199c4ce5e3a9b0b61e03a4a5a40483cc8f6ff5
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-09-12 09:57:41 +0530

    Time: 0 ms (100%), Space: 42.2 MB (12.66%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,25 +1,26 @@
 class Solution {
+    public boolean mirror(TreeNode root1, TreeNode root2) {
+        Queue<TreeNode> queue1 = new LinkedList<>();
+        queue1.add(root1);
+        Queue<TreeNode> queue2 = new LinkedList<>();
+        queue2.add(root2);
+        while(!queue1.isEmpty() && !queue2.isEmpty()) {
+            TreeNode temp1 = queue1.poll();
+            TreeNode temp2 = queue2.poll();
+            if(temp1.val != temp2.val) return false;
+            if(temp1.left != null) queue1.add(temp1.left);
+            if(temp1.right != null) queue1.add(temp1.right);
+            if(temp2.right != null) queue2.add(temp2.right);
+            if(temp2.left != null) queue2.add(temp2.left);
+            if((temp1.left!=null && temp2.right==null) || (temp1.left==null && temp2.right!=null)) return false;
+            if((temp1.right!=null && temp2.left==null) || (temp1.right==null && temp2.left!=null)) return false;
+        } return true;
+    }
+
     public boolean isSymmetric(TreeNode root) {
-        if (root == null) return true;
-
-        Queue<TreeNode> queue = new LinkedList<>();
-        queue.add(root.left);
-        queue.add(root.right);
-
-        while (!queue.isEmpty()) {
-            TreeNode t1 = queue.poll();
-            TreeNode t2 = queue.poll();
-
-            if (t1 == null && t2 == null) continue;
-            if (t1 == null || t2 == null) return false;
-            if (t1.val != t2.val) return false;
-
-            // enqueue children in mirror order
-            queue.add(t1.left);
-            queue.add(t2.right);
-            queue.add(t1.right);
-            queue.add(t2.left);
-        }
-        return true;
+        if(root == null) return true;
+        if(root.left==null && root.right==null) return true;
+        if((root.left==null && root.right!=null) || (root.left!=null && root.right==null)) return false;
+        return mirror(root.left, root.right);
     }
 }
