5aa66b9904bd7bb9b24cee1344997c2081f3d0f0
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-09-17 00:41:25 +0530

    Time: 1 ms (99.49%), Space: 56.9 MB (82.1%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,8 +4,8 @@ class Solution {
         int sum = 0;
         for(int i=0; i<nums.length; i++) {
             sum += nums[i];
-            result = Math.max(sum, result);
-            if(sum < 0) sum=0;
+            result = Math.max(sum,result);
+            if(sum<0) sum=0;
         } return result;
     }
 }
