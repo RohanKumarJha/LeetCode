fcbddc869cc3fcd48203875bb244140dd9cc8256
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-09-18 11:59:43 +0530

    Time: 1 ms (1.6%), Space: 41.6 MB (83.86%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,26 +1,31 @@
 class Solution {
+    public class Pair {
+        private TreeNode node;
+        private int value;
+        Pair(TreeNode node, int value) {
+            this.node = node;
+            this.value = value;
+        }
+    }
+
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if(root == null) return result;
-        Stack<TreeNode> st1 = new Stack<>();
-        st1.push(root);
-        Stack<Integer> st2 = new Stack<>();
-        st2.push(0);
-        while(!st1.isEmpty()) {
-            TreeNode temp = st1.pop();
-            int value = st2.pop();
-            if(value == 0) {
-                if(temp.right != null) {
-                    st1.push(temp.right);
-                    st2.push(0);
-                } st1.push(temp);
-                st2.push(1);
-                if(temp.left != null) {
-                    st1.push(temp.left);
-                    st2.push(0);
-                }
-            } else {
+        Stack<Pair> st = new Stack<>();
+        st.push(new Pair(root,0));
+        while(!st.isEmpty()) {
+            Pair pair = st.pop();
+            TreeNode temp = pair.node;
+            int value = pair.value;
+            if(value == 1) {
                 result.add(temp.val);
+            } else {
+                if(temp.right != null) {
+                    st.push(new Pair(temp.right,0));
+                } st.push(new Pair(temp,1));
+                if(temp.left != null) {
+                    st.push(new Pair(temp.left,0));
+                }
             }
         } return result;
     }
