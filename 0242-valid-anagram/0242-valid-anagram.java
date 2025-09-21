12e912707fd39a44599a9904f47a704c3082facc
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-09-21 11:41:49 +0530

    Time: 22 ms (8.77%), Space: 44.7 MB (43.59%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,12 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
         if(s.length() != t.length()) return false;
-        char[] ch1 = s.toCharArray();
-        char[] ch2 = t.toCharArray();
-        Arrays.sort(ch1);
-        Arrays.sort(ch2);
-        for(int i=0; i<ch1.length; i++) {
-            if(ch1[i] != ch2[i]) return false;
+        Map<Character, Integer> map = new HashMap<>();
+        for(int i=0; i<s.length(); i++) {
+            if(map.containsKey(s.charAt(i))) {
+                map.put(s.charAt(i), map.get(s.charAt(i))+1);
+            } else {
+                map.put(s.charAt(i), 1);
+            }
+        }
+        for(int i=0; i<t.length(); i++) {
+            if(map.containsKey(t.charAt(i))) {
+                map.put(t.charAt(i), map.get(t.charAt(i))-1);
+                if(map.get(t.charAt(i)) == 0) map.remove(t.charAt(i));
+            } else {
+                return false;
+            }
         } return true;
     }
 }
