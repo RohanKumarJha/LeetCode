9828fbececa0b3fbe92d6be64fcfae685b88151c
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-09-30 10:52:09 +0530

    Time: 1 ms (12.77%), Space: 42.3 MB (12.87%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -1,26 +1,22 @@
 class Solution {
-    public boolean mirror(TreeNode root1, TreeNode root2) {
-        Queue<TreeNode> queue1 = new LinkedList<>();
-        queue1.add(root1);
-        Queue<TreeNode> queue2 = new LinkedList<>();
-        queue2.add(root2);
-        while(!queue1.isEmpty() && !queue2.isEmpty()) {
-            TreeNode temp1 = queue1.poll();
-            TreeNode temp2 = queue2.poll();
-            if(temp1.val != temp2.val) return false;
-            if(temp1.left != null) queue1.add(temp1.left);
-            if(temp1.right != null) queue1.add(temp1.right);
-            if(temp2.right != null) queue2.add(temp2.right);
-            if(temp2.left != null) queue2.add(temp2.left);
-            if((temp1.left!=null && temp2.right==null) || (temp1.left==null && temp2.right!=null)) return false;
-            if((temp1.right!=null && temp2.left==null) || (temp1.right==null && temp2.left!=null)) return false;
-        } return true;
-    }
-
     public boolean isSymmetric(TreeNode root) {
         if(root == null) return true;
-        if(root.left==null && root.right==null) return true;
-        if((root.left==null && root.right!=null) || (root.left!=null && root.right==null)) return false;
-        return mirror(root.left, root.right);
+        Queue<TreeNode> q1 = new LinkedList<>();
+        q1.add(root.left);
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q2.add(root.right);
+
+        while(!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode temp1 = q1.poll();
+            TreeNode temp2 = q2.poll();
+            if (temp1 == null && temp2 == null) continue;
+            if(temp1==null || temp2==null) return false;
+            if(temp1.val != temp2.val) return false;
+            q1.add(temp1.left);
+            q1.add(temp1.right);
+            q2.add(temp2.right);
+            q2.add(temp2.left);
+        }
+        return q1.isEmpty() && q2.isEmpty();
     }
 }
