e7af71b1688b6a38d64c08d09a09bb02fb2104ef
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-04 08:59:13 +0530

    Time: 0 ms (100%), Space: 40.3 MB (100%) - LeetHub

--- 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.java
+++ 0799-minimum-distance-between-bst-nodes/0799-minimum-distance-between-bst-nodes.java
@@ -1,19 +1,18 @@
 class Solution {
-    Integer prev = null;
-    int minDiff = Integer.MAX_VALUE;
+    private Integer minDiff = Integer.MAX_VALUE;
+    private Integer prev = null;
+
+    private void inorder(TreeNode root) {
+        if(root == null) return;
+        inorder(root.left);
+        if(prev != null) {
+            minDiff = Math.min(minDiff,root.val-prev);
+        } prev = root.val;
+        inorder(root.right);
+    }
 
     public int minDiffInBST(TreeNode root) {
         inorder(root);
         return minDiff;
-    }
-
-    private void inorder(TreeNode root) {
-        if (root == null) return;
-        inorder(root.left);
-        if (prev != null) {
-            minDiff = Math.min(minDiff, root.val - prev);
-        }
-        prev = root.val;
-        inorder(root.right);
     }
 }
