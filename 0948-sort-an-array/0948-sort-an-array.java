a3b901947e5efcc0a85d37b0ffa3288cbf8faedd
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-04 10:06:16 +0530

    Time: 28 ms (48.52%), Space: 56.6 MB (33.23%) - LeetHub

--- 0948-sort-an-array/0948-sort-an-array.java
+++ 0948-sort-an-array/0948-sort-an-array.java
@@ -1,61 +1,41 @@
 class Solution {
-    // Merging step (conquer)
-    public void conquer(int[] nums, int start, int mid, int end) {
+    public void conquor(int[] nums,int start,int mid,int end) {
         int n1 = mid - start + 1;
         int n2 = end - mid;
 
         int[] left = new int[n1];
         int[] right = new int[n2];
 
-        for (int i = 0; i < n1; i++) {
-            left[i] = nums[start + i];
-        }
-        for (int i = 0; i < n2; i++) {
-            right[i] = nums[mid + 1 + i];
-        }
+        for (int i = 0; i < n1; i++) left[i] = nums[start + i];
+        for (int i = 0; i < n2; i++) right[i] = nums[mid + 1 + i];
 
         int i = 0, j = 0, k = start;
 
-        // Merge left and right arrays
+        // Merge back into nums
         while (i < n1 && j < n2) {
             if (left[i] <= right[j]) {
-                nums[k] = left[i];
-                i++;
+                nums[k++] = left[i++];
             } else {
-                nums[k] = right[j];
-                j++;
+                nums[k++] = right[j++];
             }
-            k++;
         }
 
         // Copy remaining elements
-        while (i < n1) {
-            nums[k] = left[i];
-            i++;
-            k++;
-        }
-
-        while (j < n2) {
-            nums[k] = right[j];
-            j++;
-            k++;
-        }
+        while (i < n1) nums[k++] = left[i++];
+        while (j < n2) nums[k++] = right[j++];
     }
 
-    // Recursive divide
-    public void divide(int[] nums, int start, int end) {
-        if (start >= end) {
-            return;
-        }
-
-        int mid = start + (end - start) / 2; // Fixed mid calculation
-        divide(nums, start, mid);
-        divide(nums, mid + 1, end);
-        conquer(nums, start, mid, end);
+    public void divide(int[] nums,int start,int end) {
+        if(start == end) return ;
+        int mid = start+(end-start)/2;
+        divide(nums,start,mid);
+        divide(nums,mid+1,end);
+        conquor(nums,start,mid,end);
     }
 
     public int[] sortArray(int[] nums) {
-        divide(nums, 0, nums.length - 1);
+        int start=0, end=nums.length-1;
+        divide(nums,start,end);
         return nums;
     }
 }
