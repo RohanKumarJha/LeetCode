473bbba1dd380dc676157da46a3f8334de0c694b
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-06 00:40:40 +0530

    Time: 12 ms (30.81%), Space: 48.4 MB (20%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,17 +1,30 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        Map<String, List<String>> ans = new HashMap<>();
-
-        for (String s : strs) {
-            char[] chars = s.toCharArray();
+        Map<String,List<String>> map = new HashMap<>();
+        for(int i=0; i<strs.length; i++) {
+            StringBuilder sb = new StringBuilder(strs[i]);
+            char[] chars = sb.toString().toCharArray();
             Arrays.sort(chars);
-            String key = new String(chars);
-            if (!ans.containsKey(key)) {
-                ans.put(key, new ArrayList<>());
+            String check = new String(chars);
+            if (map.containsKey(check)) {
+                map.get(check).add(strs[i]);
+            } else {
+                List<String> list = new ArrayList<>();
+                list.add(strs[i]);
+                map.put(check, list);
             }
-            ans.get(key).add(s);
         }
-
-        return new ArrayList<>(ans.values());        
+        List<List<String>> result = new ArrayList<>();
+        for(int i=0; i<strs.length; i++) {
+            StringBuilder sb = new StringBuilder(strs[i]);
+            char[] chars = sb.toString().toCharArray();
+            Arrays.sort(chars);
+            String check = new String(chars);
+            if (map.containsKey(check)) {
+                List<String> list = map.get(check);
+                result.add(list);
+                map.remove(check);
+            }
+        } return result;
     }
 }
