158629a7e4fc0056c8887356811cd30d26185634
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-05 11:42:51 +0530

    Time: 5 ms (95.41%), Space: 42.7 MB (81%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.java
+++ 0567-permutation-in-string/0567-permutation-in-string.java
@@ -1,23 +1,23 @@
 class Solution {
 
-    private boolean matches(int[] a, int[] b) {
-        for (int i = 0; i < 26; i++) if (a[i] != b[i]) return false;
-        return true;
+    public boolean matches(int[] arr1,int[] arr2) {
+        for(int i=0; i<26; i++) {
+            if(arr1[i] != arr2[i]) return false;
+        } return true;
     }
-    
+
     public boolean checkInclusion(String s1, String s2) {
         if(s1.length() > s2.length()) return false;
         int[] arr1 = new int[26];
         int[] arr2 = new int[26];
         for(int i=0; i<s1.length(); i++) arr1[s1.charAt(i)-'a']++;
-        for(int i=0; i<s1.length(); i++) arr2[s2.charAt(i)-'a']++;
-        if(matches(arr1,arr2)) return true;
+        for(int i=0; i<s1.length(); i++) arr2[s2.charAt(i)-'a']++; 
+        if(matches(arr1,arr2)) return true; 
         int start=0, end=s1.length();
         while(end < s2.length()) {
             arr2[s2.charAt(start++)-'a']--;
             arr2[s2.charAt(end++)-'a']++;
             if(matches(arr1,arr2)) return true;
-        }
-        return false;
+        } return false;
     }
 }
