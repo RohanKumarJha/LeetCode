b3ac6b2bb9538beb42f2d3ec5c77f46aeeaac2c8
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-08 10:04:03 +0530

    Time: 2 ms (34.18%), Space: 44.5 MB (82.85%) - LeetHub

--- 0046-permutations/0046-permutations.java
+++ 0046-permutations/0046-permutations.java
@@ -1,30 +1,20 @@
-public class Solution {
-    public List<List<Integer>> permute(int[] nums) {
-        List<List<Integer>> resultList = new ArrayList<>();
-        backtrack(resultList, new ArrayList<>(), nums);
-        return resultList;
-    }
-
-    private void backtrack(List<List<Integer>> resultList, ArrayList<Integer> tempList, int[] nums) {
-        // If we match the length, it is a permutation
-        if (tempList.size() == nums.length) {
-            resultList.add(new ArrayList<>(tempList));
+class Solution {
+    public void permut(int[] nums,List<Integer> list,List<List<Integer>> result) {
+        if(list.size()==nums.length) {
+            result.add(new ArrayList<>(list));
             return;
         }
-
-        for (int number : nums) {
-            // Skip if we get same element
-            if (tempList.contains(number)) continue;
-
-            // Add the new element
-            tempList.add(number);
-
-            // Go back to try other element
-            backtrack(resultList, tempList, nums);
-
-            // Remove the element
-            tempList.remove(tempList.size() - 1);
+        for(int i=0; i<nums.length; i++) {
+            if(list.contains(nums[i])) continue;
+            list.add(nums[i]);
+            permut(nums,list,result);
+            list.remove(list.size()-1);
         }
     }
 
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> result = new ArrayList<>();
+        permut(nums,new ArrayList<>(),result);
+        return result;
+    }
 }
