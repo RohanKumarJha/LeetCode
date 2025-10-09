445b2831afa7c3428e45d66b0a70f665e4f1090a
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-09 11:43:02 +0530

    Time: 0 ms (100%), Space: 42.1 MB (11.48%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,48 +1,41 @@
 class MyQueue {
 
-    Stack<Integer> st1;
-    Stack<Integer> st2;
+    private Stack<Integer> q1;
+    private Stack<Integer> q2;
 
     public MyQueue() {
-        st1 = new Stack<>();
-        st2 = new Stack<>();
+        q1 = new Stack<>();
+        q2 = new Stack<>();
     }
     
     public void push(int x) {
-        st1.push(x);
+        q1.push(x);
     }
     
     public int pop() {
-        while(st1.size() != 1) {
-            st2.push(st1.pop());
-        } 
-        int result = st1.pop();
-        while(st2.size() != 0) {
-            st1.push(st2.pop());
-        } return result;
+        while(q1.size() != 1) {
+            q2.push(q1.pop());
+        }
+        int result = q1.pop();
+        while(q2.size() != 0) {
+            q1.push(q2.pop());
+        }
+        return result;
     }
     
     public int peek() {
-        while(st1.size() != 1) {
-            st2.push(st1.pop());
-        } 
-        int result = st1.pop();
-        st2.push(result);
-        while(st2.size() != 0) {
-            st1.push(st2.pop());
-        } return result;
+        while(q1.size() != 1) {
+            q2.push(q1.pop());
+        }
+        int result = q1.pop();
+        q1.push(result);
+        while(q2.size() != 0) {
+            q1.push(q2.pop());
+        }
+        return result;
     }
     
     public boolean empty() {
-        return (st1.size() == 0) ? true : false;
+        return q1.isEmpty();
     }
 }
-
-/**
- * Your MyQueue object will be instantiated and called as such:
- * MyQueue obj = new MyQueue();
- * obj.push(x);
- * int param_2 = obj.pop();
- * int param_3 = obj.peek();
- * boolean param_4 = obj.empty();
- */
