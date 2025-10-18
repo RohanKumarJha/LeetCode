d2ae3bdf199326b87d6861b049fb41a4c06ad8cd
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-18 11:06:41 +0530

    Time: 0 ms (100%), Space: 45.2 MB (72.92%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,11 @@
 class Solution {
     public int missingNumber(int[] nums) {
-        int n = nums.length;
-        Arrays.sort(nums);
-        for(int i=0; i<n; i++) {
-            if(i != nums[i]) return i;
-        } return n;
+        boolean[] arr = new boolean[nums.length+1];
+        for(int i : nums) {
+            arr[i] = true;
+        }
+        for(int i=0; i<arr.length; i++) {
+            if(!arr[i]) return i;
+        } return 0;
     }
 }
