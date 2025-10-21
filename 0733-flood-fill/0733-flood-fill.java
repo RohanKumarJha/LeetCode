e952cb9c4fc112caa7d095caa900aa8148c0f131
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-21 12:02:01 +0530

    Time: 0 ms (100%), Space: 45.1 MB (52.18%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,16 +1,16 @@
 class Solution {
-    public void applyFloodFill(int[][] image, int sr, int sc, int color, int answer) {
-        if(sr==-1 || sc==-1 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=answer || image[sr][sc]==color) return ;
-        image[sr][sc] = color;
-        applyFloodFill(image, sr-1, sc, color, answer);
-        applyFloodFill(image, sr+1, sc, color, answer);
-        applyFloodFill(image, sr, sc-1, color, answer);
-        applyFloodFill(image, sr, sc+1, color, answer);
+    public void changeMatrix(int[][] image, int sr, int sc, int color,int elm) {
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]==color || 
+        image[sr][sc]!=elm) return ;
+        if(image[sr][sc]==elm) image[sr][sc]=color;
+        changeMatrix(image,sr+1,sc,color,elm);
+        changeMatrix(image,sr-1,sc,color,elm);
+        changeMatrix(image,sr,sc+1,color,elm);
+        changeMatrix(image,sr,sc-1,color,elm);
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int answer = image[sr][sc];
-        applyFloodFill(image, sr, sc, color, answer);
+        changeMatrix(image,sr,sc,color,image[sr][sc]);
         return image;
     }
 }
