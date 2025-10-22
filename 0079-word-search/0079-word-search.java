1fd4f40a6b6f4802110049299821802d694a817b
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-22 10:41:24 +0530

    Time: 134 ms (71.93%), Space: 41.7 MB (49.28%) - LeetHub

--- 0079-word-search/0079-word-search.java
+++ 0079-word-search/0079-word-search.java
@@ -1,15 +1,12 @@
 class Solution {
-    public boolean check(char[][] board, String word, int index, int pos1, int pos2) {
+    public boolean check(char[][] board, int row, int col, int index, String word) {
         if(index == word.length()) return true;
-        if(pos1<0 || pos2<0 || pos1>=board.length || pos2>=board[0].length
-        || board[pos1][pos2] != word.charAt(index)) return false;
-        char temp = board[pos1][pos2];
-        board[pos1][pos2] = '$';
-        boolean result = check(board, word, index+1, pos1+1, pos2) 
-        || check(board, word, index+1, pos1-1, pos2)
-        || check(board, word, index+1, pos1, pos2+1)
-        || check(board, word, index+1, pos1, pos2-1);
-        board[pos1][pos2] = temp;
+        if(row<0 || col<0 || row>=board.length || col>=board[0].length || board[row][col]!=word.charAt(index)) return false;
+        char temp = board[row][col];
+        board[row][col] = '$';
+        boolean result = check(board,row+1,col,index+1,word) || check(board,row-1,col,index+1,word) 
+        || check(board,row,col+1,index+1,word) || check(board,row,col-1,index+1,word);
+        board[row][col] = temp;
         return result;
     }
 
@@ -17,7 +14,7 @@ class Solution {
         for(int i=0; i<board.length; i++) {
             for(int j=0; j<board[0].length; j++) {
                 if(board[i][j] == word.charAt(0)) {
-                    boolean result = check(board,word,0,i,j);
+                    boolean result = check(board,i,j,0,word);
                     if(result) return result;
                 }
             }
