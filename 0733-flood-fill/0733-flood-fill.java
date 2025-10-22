2513f06ba9cbaaea37ec0e68568c7e627fc50026
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-22 10:21:34 +0530

    Time: 0 ms (100%), Space: 44.7 MB (97.63%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,8 +1,8 @@
 class Solution {
-    public void flood(int[][] image, int sr, int sc, int color, int result) {
+    public void flood(int[][] image, int sr, int sc, int color,int result) {
         if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=result
-        || image[sr][sc]==color) return ;
-        if(image[sr][sc]==result) image[sr][sc]=color;
+        || image[sr][sc]==color) return;
+        if(image[sr][sc] == result) image[sr][sc]=color;
         flood(image,sr+1,sc,color,result);
         flood(image,sr-1,sc,color,result);
         flood(image,sr,sc+1,color,result);
@@ -10,7 +10,8 @@ class Solution {
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        flood(image,sr,sc,color,image[sr][sc]);
+        int result = image[sr][sc];
+        flood(image,sr,sc,color,result);
         return image;
     }
 }
