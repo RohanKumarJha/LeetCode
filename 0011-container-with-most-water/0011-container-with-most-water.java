83afa7ec9176e64336b894509343e839a4a14c5a
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-24 20:05:53 +0530

    Time: 5 ms (77.8%), Space: 58 MB (44.46%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.java
+++ 0011-container-with-most-water/0011-container-with-most-water.java
@@ -3,12 +3,9 @@ class Solution {
         int result = 0;
         int start=0, end=height.length-1;
         while(start < end) {
-            result = Math.max(result,Math.min(height[end],height[start])*(end-start));
-            if(height[start] < height[end]) {
-                start++;
-            } else {
-                end--;
-            }
+            result = Math.max(Math.min(height[end],height[start])*(end-start),result);
+            if(height[start] < height[end]) start++;
+            else end--;
         } return result;
     }
 }
