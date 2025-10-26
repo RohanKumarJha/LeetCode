c3075d570d3ce6413e30714c9e62fe9040d3e701
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-26 18:20:49 +0530

    Time: 0 ms (100%), Space: 42.7 MB (33.06%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,12 +1,16 @@
 class Solution {
-    public double result(double x, long n) {
-        if(n == 0) return 1;
-        if(n < 0) return 1.0/result(x,-n);
-        if(n % 2 == 0) return result(x*x,n/2);
-        else return x*result(x*x,n/2);
+    public double pow(double x, int n) {
+        if(n == 0) return 1.0;
+        if(n % 2 == 0) {
+            return pow(x*x,n/2);
+        }
+        if(n % 2 != 0) {
+            return x*pow(x*x,n/2);
+        } 
+        return x;
     }
-    
+
     public double myPow(double x, int n) {
-        return result(x, (long)n);
+        return (n < 0) ? 1/pow(x,-n) : pow(x,n);
     }
 }
