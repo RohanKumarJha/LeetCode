7718dcbabb22f98ea75dea583037225d44e1ebe8
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-27 11:08:13 +0530

    Time: 0 ms (100%), Space: 40.6 MB (65.89%) - LeetHub

--- 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.java
@@ -1,12 +1,12 @@
 class Solution {
-    public int trailing(int n,int result) {
+    public int fact(int n,int result) {
         if(n < 5) return result;
         result += (n/5);
-        return trailing(n/5, result);
+        return fact(n/5,result);
     }
 
     public int trailingZeroes(int n) {
         int result = 0;
-        return trailing(n,result);
+        return fact(n,result);
     }
 }
