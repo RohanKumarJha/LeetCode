a4da73eab7a8a19d14cd8057fb8246e1c68a2821
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-10-31 23:30:16 +0530

    Time: 26 ms (22.14%), Space: 48 MB (5.12%) - LeetHub

--- 0907-koko-eating-bananas/0907-koko-eating-bananas.java
+++ 0907-koko-eating-bananas/0907-koko-eating-bananas.java
@@ -1,30 +1,30 @@
 class Solution {
-    public int timeTaken(int[] piles, int mid) {
-        int time = 0;
-        for (int i = 0; i < piles.length; i++) {
-            time += Math.ceil((double)piles[i] / mid);
-        }
-        return time;
-    }
-
     public int minEatingSpeed(int[] piles, int h) {
-        int maxElm = Integer.MIN_VALUE;
-        for (int i = 0; i < piles.length; i++) {
-            maxElm = Math.max(maxElm, piles[i]);
+        int maxVal = Integer.MIN_VALUE;
+        for(int i : piles) {
+            maxVal = Math.max(maxVal, i);
         }
 
-        int start = 1, end = maxElm;
-        int answer = maxElm;
-        while (start <= end) {
-            int mid = start + (end - start) / 2;
-            int totalTime = timeTaken(piles, mid);
-            if (totalTime <= h) {
-                answer = mid;
-                end = mid - 1;
-            } else {
-                start = mid + 1;
+        int start=1, end=maxVal;
+        int result = Integer.MAX_VALUE;
+        while(start <= end) {
+            int mid = start+(end-start)/2;
+            int time = h;
+            for(int i : piles) {
+                double answer = Math.ceil((double)i/mid);
+                time -= answer;
+                if(time < 0) {
+                    start = mid+1;
+                    break;
+                }
             }
-        }
-        return answer;
+            if(time >= 0) {
+                result = mid;
+                end = mid-1;
+            } else {
+                start = mid+1;
+            }
+        } return result;
+
     }
 }
