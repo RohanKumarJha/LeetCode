1e0755d7c0cea943173c44400fb186c3a80eb66b
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-05 08:21:20 +0530

    Time: 22 ms (19.07%), Space: 97.9 MB (5.41%) - LeetHub

--- 0077-combinations/0077-combinations.java
+++ 0077-combinations/0077-combinations.java
@@ -1,25 +1,19 @@
 class Solution {
-    public void comb(int[] nums,int k,int index,List<Integer> list,List<List<Integer>> result) {
+    public void comb(int n,int k,int index,List<Integer> list,List<List<Integer>> result) {
         if(list.size() == k) {
             result.add(new ArrayList<>(list));
             return ;
         }
-        if(index == nums.length) return ;
-        list.add(nums[index]);
-        comb(nums,k,index+1,list,result);
+        if(index > n) return ;
+        list.add(index);
+        comb(n,k,index+1,list,result);
         list.remove(list.size()-1);
-        comb(nums,k,index+1,list,result);
+        comb(n,k,index+1,list,result);
     }
 
     public List<List<Integer>> combine(int n, int k) {
-        int[] nums = new int[n];
-        int count = 1;
-        for(int i=0; i<n; i++) {
-            nums[i] = count;
-            count++;
-        }
         List<List<Integer>> result = new ArrayList<>();
-        comb(nums,k,0,new ArrayList<>(),result);
+        comb(n,k,1,new ArrayList<>(),result);
         return result;
     }
 }
