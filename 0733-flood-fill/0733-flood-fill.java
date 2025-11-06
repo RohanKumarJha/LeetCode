753ac789b6aa88448bb5cf263d227620ce0ed63e
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-06 12:03:37 +0530

    Time: 0 ms (100%), Space: 46.7 MB (5.28%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,17 +1,17 @@
 class Solution {
-    private void check(int[][] image, int sr, int sc, int color,int pixelColor) {
-        if(sr==-1 || sc==-1 || sr==image.length || sc==image[0].length || image[sr][sc]!=pixelColor) return ;
+    private void flood_fill(int[][] image, int sr, int sc, int color,int original_pixel) {
+        if(sr<0 || sc<0 || sr>=image.length || sc>=image[0].length || image[sr][sc]!=original_pixel) return;
         image[sr][sc] = color;
-        check(image,sr-1,sc,color,pixelColor);
-        check(image,sr+1,sc,color,pixelColor);
-        check(image,sr,sc-1,color,pixelColor);
-        check(image,sr,sc+1,color,pixelColor);
+        flood_fill(image,sr-1,sc,color,original_pixel);
+        flood_fill(image,sr+1,sc,color,original_pixel);
+        flood_fill(image,sr,sc-1,color,original_pixel);
+        flood_fill(image,sr,sc+1,color,original_pixel);
     }
-    
+
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
-        int pixelColor = image[sr][sc];
-        if(pixelColor == color) return image;
-        check(image,sr,sc,color,pixelColor);
+        int original_pixel = image[sr][sc];
+        if(color == original_pixel) return image;
+        flood_fill(image,sr,sc,color,original_pixel);
         return image;
     }
 }
