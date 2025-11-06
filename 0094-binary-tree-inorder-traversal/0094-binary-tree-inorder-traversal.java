9a1dc1c4214de656f2350aa0eac3ff685c2e8c0b
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-06 10:37:43 +0530

    Time: 0 ms (100%), Space: 43 MB (5.31%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,28 +1,14 @@
 class Solution {
-    private class CheckStack {
-        TreeNode node;
-        boolean existAgain;
-        CheckStack(TreeNode node,boolean existAgain) {
-            this.node = node;
-            this.existAgain = existAgain;
-        }
+    private void inOrder(TreeNode root,List<Integer> result) {
+        if(root.left != null) inOrder(root.left,result);
+        result.add(root.val);
+        if(root.right != null) inOrder(root.right,result);
     }
+
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
         if(root == null) return result;
-        Stack<CheckStack> st = new Stack<>();
-        st.push(new CheckStack(root,false));
-        while(!st.isEmpty()) {
-            CheckStack elm = st.pop();
-            TreeNode temp = elm.node;
-            boolean flag = elm.existAgain;
-            if(flag == true) {
-                result.add(temp.val);
-                continue;
-            }
-            if(temp.right != null) st.push(new CheckStack(temp.right,false));
-            st.push(new CheckStack(temp,true));
-            if(temp.left != null) st.push(new CheckStack(temp.left,false));
-        } return result;
+        inOrder(root,result);
+        return result;
     }
 }
