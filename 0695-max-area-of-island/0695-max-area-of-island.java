b2e7b4d62194a3e88cb7e1d70cf10721716b4449
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-06 12:12:32 +0530

    Time: 1 ms (100%), Space: 46.4 MB (5.23%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.java
+++ 0695-max-area-of-island/0695-max-area-of-island.java
@@ -1,28 +1,27 @@
 class Solution {
-    private int result = 0;
-    private int answer = 0;
+    private int max_area = 0;
+    private int area = 0;
 
-    private void choose(int[][] grid,int i,int j) {
-        if(i==-1 || j==-1 || i==grid.length || j==grid[0].length || grid[i][j]!=1) return ;
+    private void findArea(int[][] grid,int i,int j) {
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || grid[i][j]!=1) return ;
         if(grid[i][j] == 1) {
-            answer++;
+            area += 1;
             grid[i][j] = 0;
         }
-        choose(grid,i-1,j);
-        choose(grid,i+1,j);
-        choose(grid,i,j-1);
-        choose(grid,i,j+1);
+        findArea(grid,i-1,j);
+        findArea(grid,i+1,j);
+        findArea(grid,i,j-1);
+        findArea(grid,i,j+1);
     }
-
     public int maxAreaOfIsland(int[][] grid) {
         for(int i=0; i<grid.length; i++) {
             for(int j=0; j<grid[0].length; j++) {
                 if(grid[i][j] == 1) {
-                    choose(grid,i,j);
-                    result = Math.max(result,answer);
-                    answer = 0;
+                    findArea(grid,i,j);
+                    max_area = Math.max(max_area,area);
+                    area = 0;
                 }
             }
-        } return result;
+        } return max_area;
     }
 }
