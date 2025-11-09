1e324753f5943f70892d966a2b9dcf2b91cf574d
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-10 00:12:45 +0530

    Time: 0 ms (100%), Space: 44.4 MB (8.78%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -1,37 +1,45 @@
 class Solution {
+    private void swap(int[] nums,int start,int end) {
+        int temp = nums[start];
+        nums[start] = nums[end];
+        nums[end] = temp;
+    }
 
-    public void reverse(int[] nums,int start,int end) {
+    private void reverse(int[] nums,int start,int end) {
         while(start < end) {
             swap(nums,start,end);
             start++; end--;
         }
     }
 
-    public void swap(int[] nums,int start,int end) {
-        int temp = nums[start];
-        nums[start] = nums[end];
-        nums[end] = temp;
-    }
-
     public void nextPermutation(int[] nums) {
-        int size = nums.length;
-        int index = -1;
-        for(int i=size-1; i>0; i--) {
-            if(nums[i] > nums[i-1]) {
-                index = i-1;
+        int pos = -1;
+        for(int i=nums.length-1; i>0; i--) {
+            if(nums[i-1] < nums[i]) {
+                pos = i-1;
                 break;
             }
         }
-
-        if(index == -1) {
-            reverse(nums,0,size-1);
-        } else {
-            int pos = size-1;
-            while(nums[pos] <= nums[index]) {
-                pos--;
+        if(pos == -1) {
+            reverse(nums,0,nums.length-1);
+        } 
+        else {
+            int pos2 = -1;
+            for(int i=pos+1; i<nums.length; i++) {
+                if(nums[i] <= nums[pos]) {
+                    pos2 = i-1;
+                    break;
+                }
             }
-            swap(nums,index,pos);
-            reverse(nums,index+1,size-1);
+
+            if(pos2 == -1) {
+                swap(nums,pos,nums.length-1);
+                reverse(nums,pos+1,nums.length-1);
+            } else {
+                swap(nums,pos,pos2);
+                reverse(nums,pos+1,nums.length-1);
+            }
         }
+
     }
 }
