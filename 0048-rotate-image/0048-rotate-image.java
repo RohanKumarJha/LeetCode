4b012b53c53536b2f38ab2e9f1cadd3f0cc5265e
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-10 00:47:08 +0530

    Time: 0 ms (100%), Space: 43.7 MB (6.51%) - LeetHub

--- 0048-rotate-image/0048-rotate-image.java
+++ 0048-rotate-image/0048-rotate-image.java
@@ -1,5 +1,11 @@
 class Solution {
-    public void transpose(int[][] matrix,int i,int j) {
+    private void swap(int[][] matrix,int i,int j,int index) {
+        int temp = matrix[index][i];
+        matrix[index][i] = matrix[index][j];
+        matrix[index][j] = temp;
+    }
+
+    private void swap(int[][] matrix,int i,int j) {
         int temp = matrix[i][j];
         matrix[i][j] = matrix[j][i];
         matrix[j][i] = temp;
@@ -7,19 +13,15 @@ class Solution {
 
     public void rotate(int[][] matrix) {
         for(int i=0; i<matrix.length; i++) {
-            for(int j=i+1; j<matrix[0].length; j++) {
-                if(i != j) {
-                    transpose(matrix,i,j);
-                }
+            for(int j=i; j<matrix.length; j++) {
+                swap(matrix,i,j);
             }
         }
 
         for(int i=0; i<matrix.length; i++) {
-            int start=0, end=matrix[0].length-1;
+            int start=0, end=matrix.length-1;
             while(start < end) {
-                int temp = matrix[i][start];
-                matrix[i][start] = matrix[i][end];
-                matrix[i][end] = temp;
+                swap(matrix,start,end,i);
                 start++; end--;
             }
         }
