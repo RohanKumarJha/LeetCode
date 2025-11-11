509aa2f88e24744b96ddd4bdcfdb57f955550173
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-11 12:01:24 +0530

    Time: 1 ms (100%), Space: 46.2 MB (11.12%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,43 +1,23 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int remainder = 0;
-        ListNode temp = null;
-        ListNode llHead = temp;
-        while(l1!=null || l2!=null || remainder!=0) {
-            int sum = 0;
-            if(l1!=null && l2!=null) {
-                sum += (l1.val+l2.val+remainder);
-                l1 = l1.next;
-                l2 = l2.next;
-            } else if(l1!=null) {
-                sum += (l1.val+remainder);
-                l1 = l1.next;
-            } else if(l2!=null) {
-                sum += (l2.val+remainder);
-                l2 = l2.next;
-            } else {
-                sum += remainder;
-            }
+        ListNode dummyHead = new ListNode(0);
+        ListNode current = dummyHead;
+        int carry = 0;
 
-            ListNode curr = new ListNode(sum%10);
-            if(temp == null) {
-                temp = curr;
-                llHead = curr;
-            } else {
-                temp.next = curr;
-                temp = temp.next;
-            }
-            remainder = sum/10;
-        } return llHead;
+        while (l1 != null || l2 != null || carry != 0) {
+            int val1 = (l1 != null) ? l1.val : 0;
+            int val2 = (l2 != null) ? l2.val : 0;
+
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+
+            current.next = new ListNode(sum % 10);
+            current = current.next;
+
+            if (l1 != null) l1 = l1.next;
+            if (l2 != null) l2 = l2.next;
+        }
+
+        return dummyHead.next;
     }
 }
