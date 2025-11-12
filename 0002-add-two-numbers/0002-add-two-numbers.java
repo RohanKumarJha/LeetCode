3759f6ee49290ce781635bd4c16045709a951db8
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-12 19:22:46 +0530

    Time: 1 ms (100%), Space: 46.4 MB (7.22%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,23 +1,37 @@
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode current = dummyHead;
+        ListNode dummyList = new ListNode();
+        ListNode current = dummyList;
         int carry = 0;
 
-        while (l1 != null || l2 != null || carry != 0) {
-            int val1 = (l1 != null) ? l1.val : 0;
-            int val2 = (l2 != null) ? l2.val : 0;
-
-            int sum = val1 + val2 + carry;
-            carry = sum / 10;
-
-            current.next = new ListNode(sum % 10);
+        while(l1!=null || l2!=null || carry!=0) {
+            ListNode node;
+            if(l1 != null && l2 != null) {
+                int digit = l1.val+l2.val+carry;
+                node = new ListNode(digit%10);
+                carry = digit/10;
+                current.next = node;
+                l1 = l1.next;
+                l2 = l2.next;
+            } else if(l2 != null) {
+                int digit = l2.val+carry;
+                node = new ListNode(digit%10);
+                carry = digit/10;
+                current.next = node;
+                l2 = l2.next;
+            } else if(l1 != null) {
+                int digit = l1.val+carry;
+                node = new ListNode(digit%10);
+                carry = digit/10;
+                current.next = node;
+                l1 = l1.next;
+            } else {
+                int digit = carry;
+                node = new ListNode(digit%10);
+                carry = 0;
+                current.next = node;
+            }
             current = current.next;
-
-            if (l1 != null) l1 = l1.next;
-            if (l2 != null) l2 = l2.next;
-        }
-
-        return dummyHead.next;
+        } return dummyList.next;
     }
 }
