5fd26d81030a3e1706ac6862c4cffeb55ecd9da3
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-16 10:01:22 +0530

    Time: 5 ms (57.07%), Space: 46.4 MB (6.02%) - LeetHub

---
+++ 1636-number-of-substrings-with-only-1s/1636-number-of-substrings-with-only-1s.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numSub(String s) {
+        int start=0, end=0;
+        int result = 0;
+        int count = 0;
+        int mod = 1_000_000_007;
+        while(end < s.length()) {
+            if(s.charAt(end) == '0') {
+                start = end+1;
+                count = 0;
+            } else {
+                count++;
+                result += count;
+                result %= mod;
+            } 
+            end++;
+        }
+        return result;
+    }
+}
