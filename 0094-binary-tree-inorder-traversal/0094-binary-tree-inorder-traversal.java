cdf9af2f9faeeb4a377fc79dc45558b9e756ae88
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-20 23:26:24 +0530

    Time: 0 ms (100%), Space: 42.7 MB (25.96%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,29 +1,14 @@
 class Solution {
-    private class CheckVisited {
-        TreeNode val;
-        boolean isVisited;
-        CheckVisited(TreeNode val,boolean isVisited) {
-            this.val = val;
-            this.isVisited = isVisited;
-        }    
+    private void inorder(TreeNode root,List<Integer> list) {
+        if(root == null) return ;
+        inorder(root.left,list);
+        list.add(root.val);
+        inorder(root.right,list);
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        Stack<CheckVisited> st = new Stack<>();
-        if(root == null) return result;
-        st.push(new CheckVisited(root,false));
-        while(!st.isEmpty()) {
-            CheckVisited checkVisited = st.pop();
-            TreeNode node = checkVisited.val;
-            boolean isVisited = checkVisited.isVisited;
-            if(isVisited == true) {
-                result.add(node.val);
-            } else {
-                if(node.right != null) st.push(new CheckVisited(node.right,false));
-                st.push(new CheckVisited(node,true));
-                if(node.left != null) st.push(new CheckVisited(node.left,false));
-            }
-        } return result;
+        List<Integer> list = new ArrayList<>();
+        inorder(root,list);
+        return list;
     }
 }
