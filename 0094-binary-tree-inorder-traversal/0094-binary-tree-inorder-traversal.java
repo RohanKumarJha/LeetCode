b91a05e6aabb864f0a96f91e0bfe7ea60a863db3
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-20 23:45:47 +0530

    Time: 1 ms (2.9%), Space: 43.2 MB (6.16%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,13 +1,31 @@
 class Solution {
+    class Pair {
+        TreeNode node;
+        boolean flag;
+        Pair(TreeNode node,boolean flag) {
+            this.node = node;
+            this.flag = flag;
+        }
+    }
+
     private void inorder(TreeNode root,List<Integer> list) {
-        if(root == null) return ;
-        inorder(root.left,list);
-        list.add(root.val);
-        inorder(root.right,list);
+        Stack<Pair> st = new Stack<>();
+        st.push(new Pair(root,false));
+        while(!st.isEmpty()) {
+            Pair elm = st.pop();
+            if(elm.flag == true) {
+                list.add(elm.node.val);
+            } else {
+                if(elm.node.right != null) st.push(new Pair(elm.node.right,false));
+                st.push(new Pair(elm.node,true));
+                if(elm.node.left != null) st.push(new Pair(elm.node.left,false));
+            }
+        }
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
         inorder(root,list);
         return list;
     }
