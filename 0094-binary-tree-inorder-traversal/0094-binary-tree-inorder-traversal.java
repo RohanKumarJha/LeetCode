e5a600cbd462f3ea4e37abe54332ae72a0ffa5dd
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-21 09:40:28 +0530

    Time: 0 ms (100%), Space: 43.1 MB (18.62%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,32 +1,22 @@
 class Solution {
-    class Pair {
-        TreeNode node;
-        boolean flag;
-        Pair(TreeNode node,boolean flag) {
-            this.node = node;
-            this.flag = flag;
-        }
-    }
-
-    private void inorder(TreeNode root,List<Integer> list) {
-        Stack<Pair> st = new Stack<>();
-        st.push(new Pair(root,false));
-        while(!st.isEmpty()) {
-            Pair elm = st.pop();
-            if(elm.flag == true) {
-                list.add(elm.node.val);
-            } else {
-                if(elm.node.right != null) st.push(new Pair(elm.node.right,false));
-                st.push(new Pair(elm.node,true));
-                if(elm.node.left != null) st.push(new Pair(elm.node.left,false));
-            }
-        }
-    }
-
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> list = new ArrayList<>();
         if(root == null) return list;
-        inorder(root,list);
-        return list;
+
+        TreeNode curr = root;
+        while(curr != null) {
+            if(curr.left == null) {
+                list.add(curr.val);
+                curr = curr.right;
+            } else {
+                TreeNode agla = curr.left;
+                TreeNode temp = agla;
+                while(temp.right != null) {
+                    temp = temp.right;
+                } temp.right = curr;
+                curr.left = null;
+                curr = agla;
+            }
+        } return list;
     }
 }
