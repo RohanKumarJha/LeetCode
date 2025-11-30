5f5c9011c9eacab0519bfd1ea5757df66ef00f06
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-11-30 10:37:48 +0530

    Time: 0 ms (100%), Space: 42.8 MB (17.85%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -1,8 +1,30 @@
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
         if(p==null && q==null) return true;
-        if(p==null || q==null) return false;
-        if(p.val != q.val) return false;
-        return isSameTree(p.left,q.left) && isSameTree(p.right,q.right);
+        if((p!=null && q==null) || (p==null && q!=null)) return false;
+
+        Queue<TreeNode> q1 = new LinkedList<>();
+        q1.add(p);
+
+        Queue<TreeNode> q2 = new LinkedList<>();
+        q2.add(q);
+
+        while(!q1.isEmpty() && !q2.isEmpty()) {
+            TreeNode node1 = q1.remove();
+            TreeNode node2 = q2.remove();
+            if(node1.val != node2.val) return false;
+            if((node1.left!=null && node2.left==null) || (node1.left==null && node2.left!=null)) return false;
+            if((node1.right!=null && node2.right==null) || (node1.right==null && node2.right!=null)) return false;
+            if(node1.left != null) {
+                q1.add(node1.left);
+                q2.add(node2.left);
+            }
+            if(node1.right != null) {
+                q1.add(node1.right);
+                q2.add(node2.right);
+            }
+        }
+
+        return (q1.size()==0 && q2.size()==0) ? true : false;
     }
 }
