b27218dee0515f67f5b2a97cda8d7d471bbe4bc1
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-15 19:59:39 +0530

    Time: 2 ms (66.81%), Space: 42.6 MB (55.44%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.java
+++ 0392-is-subsequence/0392-is-subsequence.java
@@ -2,11 +2,9 @@ class Solution {
     public boolean isSubsequence(String s, String t) {
         if(s.length() > t.length()) return false;
         int pointer1=0, pointer2=0;
-        while(pointer2 < t.length()) {
-            if(pointer1 == s.length()) return true;
-            if(s.charAt(pointer1) == t.charAt(pointer2)) {
-                pointer1++;
-            } pointer2++;
+        while(pointer1!=s.length() && pointer2!=t.length()) {
+            if(s.charAt(pointer1) == (t.charAt(pointer2))) pointer1++;
+            pointer2++;
         }
         return (pointer1 == s.length()) ? true : false;
     }
