33cc283f49053ac0c3173c616acf1fda3d2b2436
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-16 11:50:28 +0530

    Time: 2 ms (99.77%), Space: 52.6 MB (33.09%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,35 +1,39 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int result1=0, count1=0;
-        int result2=0, count2=0;
-        
-        for (int num : nums) {
-            if (result1 == num) {
+        int n = nums.length;
+
+        int num1=nums[0], num2=nums[0];
+        int count1=0, count2=0;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] == num1) {
                 count1++;
-            } else if (result2 == num) {
+            } else if(nums[i] == num2) {
                 count2++;
-            } else if (count1 == 0) {
-                result1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                result2 = num;
-                count2 = 1;
+            } else if(count1 == 0) {
+                num1 = nums[i];
+                count1++;
+            } else if(count2 == 0) {
+                num2 = nums[i];
+                count2++;
             } else {
                 count1--;
                 count2--;
             }
         }
 
-        count1 = 0;
-        count2 = 0;
-        for(int i : nums) {
-            if(i == result1) count1++;
-            else if(i == result2) count2++;
+        count1=0;
+        count2=0;
+
+        for(int i=0; i<n; i++) {
+            if(nums[i] == num1) count1++;
+            else if(nums[i] == num2) count2++;
         }
 
         List<Integer> list = new ArrayList<>();
-        if(count1 > nums.length/3) list.add(result1);
-        if(count2 > nums.length/3) list.add(result2);
+        if(count1 > n/3) list.add(num1);
+        if(count2 > n/3) list.add(num2);
+
         return list;
     }
 }
