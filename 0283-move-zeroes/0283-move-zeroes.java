7d8e2445b26a98ef6b9cf6e4501297ce3a112251
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-17 22:52:39 +0530

    Time: 2 ms (87.27%), Space: 48 MB (9.18%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.java
+++ 0283-move-zeroes/0283-move-zeroes.java
@@ -3,10 +3,11 @@ class Solution {
         int start=0, end=0;
         while(end < nums.length) {
             if(nums[end] != 0) {
-                nums[start] = nums[end];
-                if(start != end) nums[end] = 0;
-                start++;
+                nums[start++] = nums[end];
             } end++;
+        }
+        while(start < nums.length) {
+            nums[start++] = 0;
         }
     }
 }
