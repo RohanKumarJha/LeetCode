007a44c22318c3f3fe992ab4039a61c292fed9e6
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-23 10:58:24 +0530

    Time: 0 ms (100%), Space: 42.3 MB (8.79%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,7 +1,17 @@
 class Solution {
+    private int fibonacci(int n,int[] dp) {
+        if(n <= 1) {
+            dp[n] = n;
+            return n;
+        }
+        if(dp[n] != 0) return dp[n];
+        dp[n] = fibonacci(n-1,dp) + fibonacci(n-2,dp);
+        return dp[n];
+    }
+
     public int fib(int n) {
-        if(n == 0) return 0;
-        if(n == 1) return 1;
-        return fib(n-1)+fib(n-2);
+        int[] dp = new int[n+1];
+        fibonacci(n,dp);
+        return dp[n];
     }
 }
