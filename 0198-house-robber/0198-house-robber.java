d0bef38e0c8c9ccecff4fbae3a7dcf849d6402c0
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-24 10:55:42 +0530

    Time: 0 ms (100%), Space: 43 MB (10.94%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,14 @@
 class Solution {
-    private int recur(int[] nums,int n,int dp[]) {
-        if(n < 0) {
-            return 0;
-        }
-        if(dp[n] != -1) return dp[n];
-        int robb = nums[n] + recur(nums,n-2,dp);
-        int skip = recur(nums,n-1,dp);
-        dp[n] = Math.max(robb, skip);
-        return dp[n];
-    }
     public int rob(int[] nums) {
         int n = nums.length;
-        int dp[] = new int[n+1];
-        Arrays.fill(dp,-1);
-        return recur(nums,n-1,dp);
+        int[] dp = new int[n+1];
+        dp[0] = nums[0];
+        if(n > 1) dp[1] = Math.max(nums[0],nums[1]);
+        for(int i=2; i<n; i++) {
+            int include = nums[i] + dp[i-2];
+            int skip = dp[i-1];
+            dp[i] = Math.max(include,skip);
+        }
+        return dp[n-1];
     }
 }
