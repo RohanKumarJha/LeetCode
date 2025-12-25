23b0475ed0d0f80a597c70a76da8d22decc5eba1
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-25 11:50:51 +0530

    Time: 9 ms (37.9%), Space: 42.1 MB (23.04%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,28 +1,6 @@
 class Solution {
-    // Using tabulation
-    private void tabulation(int n,int[] dp) {
-        for(int i=2; i<=n; i++) {
-            dp[i] = dp[i-1]+dp[i-2];
-        }
-    }
-
-    // Using memoization
-    private int fibonacci(int n,int[] dp) {
-        if(n <= 1) {
-            dp[n] = n;
-            return n;
-        }
-        if(dp[n] != 0) return dp[n];
-        dp[n] = fibonacci(n-1,dp) + fibonacci(n-2,dp);
-        return dp[n];
-    }
-
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        // fibonacci(n,dp);
-        dp[0] = 0;
-        if(n>=1) dp[1] = 1;
-        tabulation(n,dp);
-        return dp[n];
+        if(n < 2) return n;
+        return fib(n-1) + fib(n-2);
     }
 }
