e591b590cde7c23a55420a56012bee918acd1874
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-26 09:16:12 +0530

    Time: 0 ms (100%), Space: 42.3 MB (10.92%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,4 +1,15 @@
 class Solution {
+
+    private int tabu(int n,int[] dp) {
+        for(int i=0; i<n; i++) {
+            if(i < 2) {
+                dp[i] = i+1;
+            } else {
+                dp[i] = dp[i-1] + dp[i-2];
+            }
+        } return dp[n-1];
+    }
+
     private int memo(int n,int[] dp) {
         if(n < 3) {
             dp[n-1] = n;
@@ -12,6 +23,7 @@ class Solution {
     public int climbStairs(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return memo(n,dp);
+        // return memo(n,dp);
+        return tabu(n,dp);
     }
 }
