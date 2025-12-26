6e5f99335799755027cf0b5aa493093ec0c7aa03
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-26 09:47:43 +0530

    Time: 0 ms (100%), Space: 41.4 MB (88.77%) - LeetHub

--- 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
+++ 1137-n-th-tribonacci-number/1137-n-th-tribonacci-number.java
@@ -1,21 +1,11 @@
 class Solution {
-    private int  memo(int n,int[] dp) {
-        if(n < 2) {
-            dp[n] = n;
-            return n;
-        }
-        if(n == 2) {
-            dp[n] = 1;
-            return 1;
-        }
-        if(dp[n] != -1) return dp[n];
-        dp[n] = memo(n-1,dp) + memo(n-2,dp) + memo(n-3,dp);
-        return dp[n];
-    }
-
     public int tribonacci(int n) {
         int[] dp = new int[n+1];
         Arrays.fill(dp,-1);
-        return memo(n,dp);
+        for(int i=0; i<=n; i++) {
+            if(i < 2) dp[i]=i;
+            else if(i == 2) dp[i]=1;
+            else dp[i] = dp[i-1] + dp[i-2] + dp[i-3];
+        } return dp[n];
     }
 }
