cee43a2a184c319b60ddd2de2355dae8bd82febf
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2025-12-26 08:45:40 +0530

    Time: 0 ms (100%), Space: 42.1 MB (23.84%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -4,7 +4,21 @@ class Solution {
         return recur(n-1) + recur(n-2);
     }
 
+    private int memo(int n,int[] dp) {
+        if(n < 2) {
+            dp[n] = n;
+            return n;
+        }
+        if(dp[n] != -1) return dp[n];
+        dp[n] = memo(n-1,dp) + memo(n-2,dp);
+        return dp[n];
+    }
+
     public int fib(int n) {
-        return recur(n);
+        // return recur(n);
+
+        int[] dp = new int[n+1];
+        Arrays.fill(dp,-1);
+        return memo(n,dp);
     }
 }
