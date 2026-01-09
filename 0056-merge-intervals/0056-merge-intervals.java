f9159f76fdcea7462d9a9c75233df05bc9b4c3dc
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-01-09 11:32:36 +0530

    Time: 10 ms (24.6%), Space: 48.9 MB (71.45%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,30 +1,27 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
-
-        List<List<Integer>> list = new ArrayList<>();
-
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-
-        for (int i = 1; i < intervals.length; i++) {
-            if (intervals[i][0] <= end) {
-                end = Math.max(end, intervals[i][1]);
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0],b[0]));
+        int first = intervals[0][0];
+        int last = intervals[0][1];
+        List<List<Integer>> result = new ArrayList<>();
+        for(int i=1; i<intervals.length; i++) {
+            if(intervals[i][0] <= last) {
+                last = Math.max(last,intervals[i][1]);
             } else {
-                list.add(Arrays.asList(start, end));
-                start = intervals[i][0];
-                end = intervals[i][1];
+                result.add(Arrays.asList(first,last));
+                first = intervals[i][0];
+                last = intervals[i][1];
             }
         }
-        list.add(Arrays.asList(start, end));
+        result.add(Arrays.asList(first,last));
 
-        // 🔽 Convert List<List<Integer>> to int[][]
-        int[][] result = new int[list.size()][2];
-        for (int i = 0; i < list.size(); i++) {
-            result[i][0] = list.get(i).get(0);
-            result[i][1] = list.get(i).get(1);
+        int[][] arr = new int[result.size()][2];
+        for (int i = 0; i < result.size(); i++) {
+            arr[i][0] = result.get(i).get(0);
+            arr[i][1] = result.get(i).get(1);
         }
 
-        return result;
+        return arr;
+
     }
 }
