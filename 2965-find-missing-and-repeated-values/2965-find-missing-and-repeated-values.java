05f2140025ae5aec6f643d98a7305383f7f98281
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-01-18 18:37:48 +0530

    Time: 85 ms (5.08%), Space: 47.5 MB (30.48%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,27 +1,17 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        Set<Integer> set = new HashSet<>();
-        int missing_number = 0;
-        int repeating_number = 0;
-        long sum = 0;
-        for(int i=0; i<grid.length; i++) {
-            for(int j=0; j<grid[0].length; j++) {
-                if(set.contains(grid[i][j])) {
-                    repeating_number = grid[i][j];
-                    sum -= repeating_number;
-                } else {
-                    set.add(grid[i][j]);
+        int[] result = new int[2];
+        int size = grid.length*grid.length;
+        for(int i=1; i<=size; i++) {
+            int count = 0;
+            for(int j=0; j<grid.length; j++) {
+                for(int k=0; k<grid.length; k++) {
+                    if(grid[j][k] == i) count++;
                 }
-                sum += grid[i][j];
             }
+            if(count == 2) result[0]=i;
+            if(count == 0) result[1]=i;
         }
-
-        long acutal_sum = grid.length*grid.length*(grid.length*grid.length+1)/2;
-        missing_number = (int) (acutal_sum - sum);
-
-        int[] arr = new int[2];
-        arr[0] = repeating_number;
-        arr[1] = missing_number;
-        return arr;
+        return result;
     }
 }
