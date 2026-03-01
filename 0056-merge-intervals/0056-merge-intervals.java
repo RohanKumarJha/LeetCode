e60d7922e485f60d590454ab9ff9269e9cca7241
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-03-01 09:20:37 +0530

    Time: 9 ms (38.57%), Space: 48.8 MB (91.44%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,20 +1,25 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
         List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         int start = intervals[0][0];
         int end = intervals[0][1];
-        int row = 1;
-        while(row < intervals.length) {
-            if(intervals[row][0] <= end) {
-                end = Math.max(end,intervals[row][1]);
+        for(int i=1; i<intervals.length; i++) {
+            if(intervals[i][0] <= end) {
+                end = Math.max(end,intervals[i][1]);
             } else {
-                result.add(new ArrayList<>(Arrays.asList(start,end)));
-                start = intervals[row][0];
-                end = intervals[row][1];
-            } row++;
+                List<Integer> list = new ArrayList<>();
+                list.add(start);
+                list.add(end);
+                result.add(new ArrayList<>(list));
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
         }
-        result.add(new ArrayList<>(Arrays.asList(start, end)));
+        List<Integer> list = new ArrayList<>();
+        list.add(start);
+        list.add(end);
+        result.add(new ArrayList<>(list));
 
         int[][] array = new int[result.size()][2];
         for (int i = 0; i < result.size(); i++) {
@@ -23,6 +28,5 @@ class Solution {
         }
 
         return array;
-
     }
 }
