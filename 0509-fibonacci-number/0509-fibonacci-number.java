6fff220fea0b85fa5bb3dfb08507f8860739c721
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-04-11 10:19:17 +0530

    Time: 9 ms (44.09%), Space: 42.1 MB (58%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,34 +1,6 @@
 class Solution {
-    private int recur(int n) {
-        if(n < 2) return n;
-        return recur(n-1) + recur(n-2);
-    }
-
-    private int memo(int n,int[] dp) {
-        if(n < 2) {
-            dp[n] = n;
-            return n;
-        }
-        if(dp[n] != -1) return dp[n];
-        dp[n] = memo(n-1,dp) + memo(n-2,dp);
-        return dp[n];
-    }
-
-    private int tabu(int n,int[] dp) {
-        for(int i=0; i<=n; i++) {
-            if(i < 2) {
-                dp[i] = i;
-            } else {
-                dp[i] = dp[i-1] + dp[i-2];
-            }
-        } return dp[n];
-    }
-
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        // return recur(n);
-        // Arrays.fill(dp,-1);
-        // return memo(n,dp);
-        return tabu(n,dp);
+        if(n < 2) return n;
+        return fib(n-1) + fib(n-2);
     }
 }
