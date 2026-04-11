b1e60014ce20307ee96ba138caf0adbd246fa455
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-04-11 10:09:43 +0530

    Time: 0 ms (100%), Space: 42 MB (77.65%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -2,12 +2,11 @@ class Solution {
     private int climb(int n, int[] dp) {
         if(n <= 2) return n;
         if(dp[n] != 0) return dp[n];
-        dp[n] = climb(n-1, dp) + climb(n-2, dp);
+        dp[n] = climb(n-1, dp) + climb(n-2,dp);
         return dp[n];
     }
 
     public int climbStairs(int n) {
-        int[] dp = new int[n+1];
-        return climb(n,dp);
+        return climb(n,new int[n+1]);
     }
 }
