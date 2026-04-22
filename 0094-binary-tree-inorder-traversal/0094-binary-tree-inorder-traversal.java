6d08a80fef7794cdb78f1f277407b592d8c88302
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-04-22 11:52:06 +0530

    Time: 1 ms (6.14%), Space: 43.2 MB (34.32%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,16 +1,40 @@
 class Solution {
-    private List<Integer> inorder(TreeNode node, List<Integer> result) {
-        if(node == null) {
-            return result;
+
+    class TreeFlag {
+        private TreeNode node;
+        private boolean flag;
+
+        TreeFlag(TreeNode node, boolean flag) {
+            this.node = node;
+            this.flag = flag;
         }
-        inorder(node.left, result);
-        result.add(node.val);
-        inorder(node.right, result);
+    }
+
+    private List<Integer> inorder(TreeNode node, List<Integer> result) {
+        Stack<TreeFlag> st = new Stack<>();
+        st.push(new TreeFlag(node,false));
+        
+        while(!st.isEmpty()) {
+            TreeFlag treeFlag = st.pop();
+            if(treeFlag.flag == true) {
+                result.add(treeFlag.node.val);
+            } else {
+                if(treeFlag.node.right != null) {
+                    st.push(new TreeFlag(treeFlag.node.right, false));
+                }
+                st.push(new TreeFlag(treeFlag.node, true));
+                if(treeFlag.node.left != null) {
+                    st.push(new TreeFlag(treeFlag.node.left, false));
+                }
+            }
+        }
+
         return result;
     }
 
     public List<Integer> inorderTraversal(TreeNode root) {
         List<Integer> result = new ArrayList<>();
+        if(root == null) return result;
         return inorder(root, result);
     }
 }
