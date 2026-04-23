750c9b1e17101bf8b8e23cff11d91a2966421f23
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-04-23 10:23:47 +0530

    Time: 2 ms (13.11%), Space: 45 MB (67.02%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,14 +1,14 @@
-
 class Solution {
-    public void inorder(TreeNode node, List<Integer> list) {
-        if(node == null) return;
-        inorder(node.left, list);
+    private void inorder(TreeNode node,List<Integer> list) {
+        if(node == null) return ;
+        inorder(node.left,list);
         list.add(node.val);
-        inorder(node.right, list);
+        inorder(node.right,list);
     }
+
     public boolean isValidBST(TreeNode root) {
         List<Integer> list = new ArrayList<>();
-        inorder(root, list);
+        inorder(root,list);
         for(int i=1; i<list.size(); i++) {
             if(list.get(i) <= list.get(i-1)) return false;
         }
