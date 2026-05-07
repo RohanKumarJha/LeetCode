34e17f87c3b24e01238b40399b3be9b1d5d245eb
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-05-07 09:54:42 +0530

    Time: 1 ms (6.14%), Space: 43.4 MB (14.67%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,40 +1,36 @@
 class Solution {
-
-    class TreeFlag {
+    class CheckTree {
         private TreeNode node;
         private boolean flag;
 
-        TreeFlag(TreeNode node, boolean flag) {
+        public CheckTree(TreeNode node, boolean flag) {
             this.node = node;
             this.flag = flag;
         }
     }
 
-    private List<Integer> inorder(TreeNode node, List<Integer> result) {
-        Stack<TreeFlag> st = new Stack<>();
-        st.push(new TreeFlag(node,false));
-        
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<>();
+        if(root == null) return list;
+        Stack<CheckTree> st = new Stack<>();
+        st.push(new CheckTree(root,false));
+
         while(!st.isEmpty()) {
-            TreeFlag treeFlag = st.pop();
-            if(treeFlag.flag == true) {
-                result.add(treeFlag.node.val);
+            CheckTree nodeTree = st.pop();
+            TreeNode node = nodeTree.node;
+            boolean flag = nodeTree.flag;
+            if (flag) {
+                list.add(node.val);
             } else {
-                if(treeFlag.node.right != null) {
-                    st.push(new TreeFlag(treeFlag.node.right, false));
+                if (node.right != null) {
+                    st.push(new CheckTree(node.right, false));
                 }
-                st.push(new TreeFlag(treeFlag.node, true));
-                if(treeFlag.node.left != null) {
-                    st.push(new TreeFlag(treeFlag.node.left, false));
+                st.push(new CheckTree(node, true));
+                if (node.left != null) {
+                    st.push(new CheckTree(node.left, false));
                 }
             }
         }
-
-        return result;
-    }
-
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> result = new ArrayList<>();
-        if(root == null) return result;
-        return inorder(root, result);
+        return list;
     }
 }
