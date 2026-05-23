e9b1ecc556c43c92819d94d4a2458b8aaafebcba
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-05-23 08:37:40 +0530

    Time: 86 ms (5.14%), Space: 47.3 MB (58.73%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,19 +1,20 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        long size = grid.length * grid.length;
-        long actual_sum = size*(size+1)/2;
-        long actual_square_sum = size*(size+1)*(2*size+1)/6;
-        long sum = 0;
-        long square_sum = 0;
-        for(int i=0; i<grid.length; i++) {
-            for(int j=0; j<grid.length; j++) {
-                sum += grid[i][j];
-                square_sum += (grid[i][j]*grid[i][j]);
+        int size = grid.length;
+        int[] result = new int[2];
+        for(int value=1; value<=size*size; value++) {
+            int count = 0;
+            for(int i=0; i<size; i++) {
+                for(int j=0; j<size; j++) {
+                    if(grid[i][j] == value) count+=1;
+                }
+            }
+            if(count == 0) {
+                result[1] = value;
+            } else if(count == 2) {
+                result[0] = value;
             }
         }
-        long missing = ((actual_sum - sum) + ((actual_square_sum-square_sum)/(actual_sum-sum)))/2;
-        long duplicate = missing - (actual_sum-sum);
-
-        return new int[]{(int)duplicate,(int)missing};
+        return result;
     }
 }
