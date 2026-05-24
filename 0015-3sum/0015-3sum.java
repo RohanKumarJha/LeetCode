d8551764dc0b6a4dff5d2fa4abdf23b2d1373d08
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-05-24 11:36:29 +0530

    Time: 34 ms (60.16%), Space: 59.4 MB (32.44%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,29 +1,52 @@
 class Solution {
+
     public List<List<Integer>> threeSum(int[] nums) {
+
         List<List<Integer>> result = new ArrayList<>();
+
         Arrays.sort(nums);
-        int size = nums.length;
-        for(int i=0; i<size-2; i++) {
-            if(i>0 && nums[i]==nums[i-1]) continue;
-            int start=i+1, end=size-1;
-            while(start < end) {
+
+        int n = nums.length;
+
+        for (int i = 0; i < n - 2; i++) {
+
+            // Skip duplicate i
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+
+            int start = i + 1;
+            int end = n - 1;
+
+            while (start < end) {
+
                 int sum = nums[i] + nums[start] + nums[end];
-                List<Integer> list = new ArrayList<>();
-                if(sum == 0) {
-                    list.add(nums[i]);
-                    list.add(nums[start]);
-                    list.add(nums[end]);
-                    result.add(new ArrayList<>(list));
-                    start++; end--;
-                    while(start<end && nums[start]==nums[start-1]) start++;
-                    while(start<end && nums[end]==nums[end+1]) end--;
-                } else if(sum < 0) {
+
+                if (sum == 0) {
+
+                    result.add(Arrays.asList(nums[i], nums[start], nums[end]));
+
+                    start++;
+                    end--;
+
+                    // Skip duplicate start values
+                    while (start < end && nums[start] == nums[start - 1]) {
+                        start++;
+                    }
+
+                    // Skip duplicate end values
+                    while (start < end && nums[end] == nums[end + 1]) {
+                        end--;
+                    }
+
+                } else if (sum < 0) {
                     start++;
                 } else {
                     end--;
                 }
             }
         }
+
         return result;
     }
 }
