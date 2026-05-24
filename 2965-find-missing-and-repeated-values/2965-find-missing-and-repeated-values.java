1eb891487212fd089469e4ac0b43a800d9618980
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-05-24 11:03:26 +0530

    Time: 1 ms (100%), Space: 47.1 MB (88.33%) - LeetHub

--- 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
+++ 2965-find-missing-and-repeated-values/2965-find-missing-and-repeated-values.java
@@ -1,20 +1,35 @@
 class Solution {
     public int[] findMissingAndRepeatedValues(int[][] grid) {
-        int size = grid.length;
-        int[] result = new int[2];
-        for(int value=1; value<=size*size; value++) {
-            int count = 0;
-            for(int i=0; i<size; i++) {
-                for(int j=0; j<size; j++) {
-                    if(grid[i][j] == value) count+=1;
-                }
-            }
-            if(count == 0) {
-                result[1] = value;
-            } else if(count == 2) {
-                result[0] = value;
+
+        int n = grid.length;
+        int total = n * n;
+
+        long actualSum = (long) total * (total + 1) / 2;
+        long actualSqSum = (long) total * (total + 1) * (2L * total + 1) / 6;
+
+        long currSum = 0;
+        long currSqSum = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                currSum += grid[i][j];
+                currSqSum += (long) grid[i][j] * grid[i][j];
             }
         }
-        return result;
+
+        long diff = currSum - actualSum;  
+        // repeated - missing
+
+        long sqDiff = currSqSum - actualSqSum;
+        // repeated² - missing²
+
+        long sum = sqDiff / diff;
+        // repeated + missing
+
+        int repeated = (int) ((diff + sum) / 2);
+        int missing = (int) (sum - repeated);
+
+        return new int[]{repeated, missing};
     }
 }
