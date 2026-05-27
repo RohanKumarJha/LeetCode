d244c94560dd3ccc253dd09589eb3420f7a3ac0c
Rohan Kumar Jha <rohan.jha0107@gmail.com>
2026-05-27 23:04:12 +0530

    Time: 0 ms (100%), Space: 43.9 MB (26.87%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,7 +1,7 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int start = 0, end=nums.length-1;
-
+        int size = nums.length;
+        int start=0, end=size-1;
         while(start <= end) {
             int mid = start + (end-start)/2;
             if(nums[mid] == target) return mid;
@@ -9,7 +9,7 @@ class Solution {
                 if(nums[start]<=target && target<=nums[mid]) {
                     end = mid;
                 } else {
-                    start = mid+1;
+                    start = mid + 1;
                 }
             } else {
                 if(nums[mid]<=target && target<=nums[end]) {
